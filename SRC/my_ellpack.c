db45cc7169af50ed764e9590907fb4b7e0357615
Moritz Kreutzer <unrza317@lenny.rrze.uni-erlangen.de>
2011-11-16 10:00:48 +0100

    implemented padded JDS with generic row padding

--- SRC/my_ellpack.c
+++ SRC/my_ellpack.c
@@ -10,98 +10,99 @@
 
 void getPadding(int nRows, int* paddedRows) {
 
-    /* determine padding of rowlength in ELR format to achieve half-warp alignment */
+	/* determine padding of rowlength in ELR format to achieve half-warp alignment */
 
-	  int padBlock = 16*sizeof(double);
+	int padBlock = 16*sizeof(double);
 
-	  if(  nRows % padBlock != 0) {
-	  	*paddedRows = nRows + padBlock - nRows % padBlock;
-	  } else {
-      *paddedRows = nRows;
-    }
+	if(  nRows % padBlock != 0) {
+		*paddedRows = nRows + padBlock - nRows % padBlock;
+	} else {
+		*paddedRows = nRows;
+	}
 }
 
 
 /**********************  ELR MATRIX TYPE *********************************/
 
 ELR_TYPE* convertCRSToELRMatrix(  const double* crs_val, const int* crs_col, 
-                                  const int* crs_row_ptr, const int nRows) {
+		const int* crs_row_ptr, const int nRows) {
 
-    /* allocate and fill ELR-format matrix from CRS format data;
-     * elements in row retain CRS order (usually sorted by column);*/
+	/* allocate and fill ELR-format matrix from CRS format data;
+	 * elements in row retain CRS order (usually sorted by column);*/
 
-    JD_SORT_TYPE* rowSort;
-    int i, j, e, pos, rowMaxEnt, padRows;
-    size_t size_val, size_col, size_rowlen;
-    int *rowLen, *col;
-    double* val;
-    ELR_TYPE* elr = NULL;
+	JD_SORT_TYPE* rowSort;
+	int i, j, e, pos, rowMaxEnt, padRows;
+	size_t size_val, size_col, size_rowlen;
+	int *rowLen, *col;
+	double* val;
+	ELR_TYPE* elr = NULL;
 
-    /* get max number of entries in one row ###########################*/
-    rowSort = (JD_SORT_TYPE*) allocateMemory( nRows * sizeof( JD_SORT_TYPE ),
-     "rowSort" );
-     
-    for( i = 0; i < nRows; i++ ) {
-      rowSort[i].row = i;
-      rowSort[i].nEntsInRow = 0;
-    } 
+	/* get max number of entries in one row ###########################*/
+	rowSort = (JD_SORT_TYPE*) allocateMemory( nRows * sizeof( JD_SORT_TYPE ),
+			"rowSort" );
 
-    /* count entries per row ################################################## */
-    for( i = 0; i < nRows; i++) rowSort[i].nEntsInRow = crs_row_ptr[i+1] - crs_row_ptr[i];
+	for( i = 0; i < nRows; i++ ) {
+		rowSort[i].row = i;
+		rowSort[i].nEntsInRow = 0;
+	} 
 
-    /* sort rows with desceding number of NZEs ################################ */
-    qsort( rowSort, nRows, sizeof( JD_SORT_TYPE  ), compareNZEPerRow );
-    
-    rowMaxEnt = rowSort[0].nEntsInRow;
-    free( rowSort );
-    
-    /* allocate memory ################################################*/
-    elr = (ELR_TYPE*) allocateMemory( sizeof( ELR_TYPE ), "elr");
-    padRows = nRows;
-    //#ifdef PADDING
-    getPadding(nRows, &padRows);
-    IF_DEBUG(1)  printf("convertCRS to ELR: padding: \t nRows=%i to %i\n", nRows, padRows);
-    //#endif
-    
-    size_val    = (size_t) sizeof(double) * padRows * rowMaxEnt;
-    size_col    = (size_t) sizeof(int) * padRows * rowMaxEnt;
-    size_rowlen = (size_t) sizeof(int) * nRows;
+	/* count entries per row ################################################## */
+	for( i = 0; i < nRows; i++) 
+	  rowSort[i].nEntsInRow = crs_row_ptr[i+1] - crs_row_ptr[i];
 
-    rowLen = (int*)   allocHostMemory( size_rowlen ); 
-    col   = (int*)    allocHostMemory( size_col ); 
-    val   = (double*) allocHostMemory( size_val ); 
+	/* sort rows with desceding number of NZEs ################################ */
+	qsort( rowSort, nRows, sizeof( JD_SORT_TYPE  ), compareNZEPerRow );
 
-    /* initialize values ########################################### */
-    elr->rowLen = rowLen;
-    elr->col = col;
-    elr->val = val;
-    elr->nRows 	= nRows;
-    elr->nMaxRow = rowMaxEnt;
-    elr->padding = padRows;
-    
-    /* fill with zeros ############################################ */
-    for( j=0; j < elr->nMaxRow; ++j) {
-      for( i=0; i < padRows; ++i) {
-        elr->col[i+j*padRows] = 0;
-        elr->val[i+j*padRows] = 0.0;
-      }
-    }
-    
-    for( i=0; i < elr->nRows; ++i) {
-      elr->rowLen[i] = crs_row_ptr[i+1] - crs_row_ptr[i];
-  	}
+	rowMaxEnt = rowSort[0].nEntsInRow;
+	free( rowSort );
 
-    /* copy values ################################################ */
-    for( i = 0; i < nRows; ++i) {
+	/* allocate memory ################################################*/
+	elr = (ELR_TYPE*) allocateMemory( sizeof( ELR_TYPE ), "elr");
+	padRows = nRows;
+	//#ifdef PADDING
+	getPadding(nRows, &padRows);
+	IF_DEBUG(1)  printf("convertCRS to ELR: padding: \t nRows=%i to %i\n", nRows, padRows);
+	//#endif
 
-      for( j = 0; j < elr->rowLen[i]; ++j) {
-        elr->col[ j*padRows+i ]   = crs_col[ crs_row_ptr[i]+j ];
-        elr->val[ j*padRows+i ]   = crs_val[ crs_row_ptr[i]+j ];
-      }
-    }
+	size_val    = (size_t) sizeof(double) * padRows * rowMaxEnt;
+	size_col    = (size_t) sizeof(int) * padRows * rowMaxEnt;
+	size_rowlen = (size_t) sizeof(int) * nRows;
+
+	rowLen = (int*)   allocHostMemory( size_rowlen ); 
+	col   = (int*)    allocHostMemory( size_col ); 
+	val   = (double*) allocHostMemory( size_val ); 
+
+	/* initialize values ########################################### */
+	elr->rowLen = rowLen;
+	elr->col = col;
+	elr->val = val;
+	elr->nRows 	= nRows;
+	elr->nMaxRow = rowMaxEnt;
+	elr->padding = padRows;
+
+	/* fill with zeros ############################################ */
+	for( j=0; j < elr->nMaxRow; ++j) {
+		for( i=0; i < padRows; ++i) {
+			elr->col[i+j*padRows] = 0;
+			elr->val[i+j*padRows] = 0.0;
+		}
+	}
+
+	for( i=0; i < elr->nRows; ++i) {
+		elr->rowLen[i] = crs_row_ptr[i+1] - crs_row_ptr[i];
+	}
+
+	/* copy values ################################################ */
+	for( i = 0; i < nRows; ++i) {
+
+		for( j = 0; j < elr->rowLen[i]; ++j) {
+			elr->col[ j*padRows+i ]   = crs_col[ crs_row_ptr[i]+j ];
+			elr->val[ j*padRows+i ]   = crs_val[ crs_row_ptr[i]+j ];
+		}
+	}
 
 
-    return elr;
+	return elr;
 }
 
 
@@ -109,35 +110,35 @@ ELR_TYPE* convertCRSToELRMatrix(  const double* crs_val, const int* crs_col,
 
 
 void checkCRSToELRsanity(	const double* crs_val, const int* crs_col, 
-                            const int* crs_row_ptr, const int nRows,
-                            const ELR_TYPE* elr) {
-  /* check if matrix in elr is consistent with CRS;
-   * assume FORTRAN numbering in crs, C numbering in ELR */
+		const int* crs_row_ptr, const int nRows,
+		const ELR_TYPE* elr) {
+	/* check if matrix in elr is consistent with CRS;
+	 * assume FORTRAN numbering in crs, C numbering in ELR */
 
 	int i,j, hlpi;
 	int me, ierr;
-	
-	ierr = MPI_Comm_rank(MPI_COMM_WORLD, &me);
-	
-	printf("PE%i: -- ELRcopy sanity check:\n", me);
-    for (i=0; i<nRows; i++){
-      if( (crs_row_ptr[i+1] - crs_row_ptr[i]) != elr->rowLen[i]) 
-        printf("PE%i: wrong number of entries in row %i:\t %i | %i\n", me, i, 
-          crs_row_ptr[i+1] - crs_row_ptr[i], elr->rowLen[i]);
 
-      hlpi = 0;
-      for (j=crs_row_ptr[i]; j<crs_row_ptr[i+1]; j++){
-        if( crs_val[j] != elr->val[i+hlpi*elr->padding]) 
-          printf("PE%i: value mismatch [%i,%i]:\t%e | %e\n",
-			me, i,hlpi, crs_val[j], elr->val[i+hlpi*elr->padding]);
-        if( crs_col[j] != elr->col[i+hlpi*elr->padding]) 
-          printf("PE%i: index mismatch [%i,%i]:\t%i | %i\n",
-			me, i,hlpi, crs_col[j], elr->col[i+hlpi*elr->padding]);
-        hlpi++;
-      }
-    }
-   
-   printf("PE%i: -- finished sanity check.\n", me);
+	ierr = MPI_Comm_rank(MPI_COMM_WORLD, &me);
+
+	printf("PE%i: -- ELRcopy sanity check:\n", me);
+	for (i=0; i<nRows; i++){
+		if( (crs_row_ptr[i+1] - crs_row_ptr[i]) != elr->rowLen[i]) 
+			printf("PE%i: wrong number of entries in row %i:\t %i | %i\n", me, i, 
+					crs_row_ptr[i+1] - crs_row_ptr[i], elr->rowLen[i]);
+
+		hlpi = 0;
+		for (j=crs_row_ptr[i]; j<crs_row_ptr[i+1]; j++){
+			if( crs_val[j] != elr->val[i+hlpi*elr->padding]) 
+				printf("PE%i: value mismatch [%i,%i]:\t%e | %e\n",
+						me, i,hlpi, crs_val[j], elr->val[i+hlpi*elr->padding]);
+			if( crs_col[j] != elr->col[i+hlpi*elr->padding]) 
+				printf("PE%i: index mismatch [%i,%i]:\t%i | %i\n",
+						me, i,hlpi, crs_col[j], elr->col[i+hlpi*elr->padding]);
+			hlpi++;
+		}
+	}
+
+	printf("PE%i: -- finished sanity check.\n", me);
 }
 
 
@@ -146,17 +147,17 @@ void checkCRSToELRsanity(	const double* crs_val, const int* crs_col,
 
 void resetELR( ELR_TYPE* elr ) {
 
-  /* set col, val and rowLen in elr to 0 */
+	/* set col, val and rowLen in elr to 0 */
 
-  int i,j;
+	int i,j;
 
-  for(i = 0; i < elr->nRows; ++i) {
-    for(j = 0; j < elr->nMaxRow; ++j) {
-      elr->col[i+j*elr->padding] = 0;
-      elr->val[i+j*elr->padding] = 0.0;
-    }
-  }
-  for(i = 0; i < elr->nRows; ++i) elr->rowLen[i] = 0;
+	for(i = 0; i < elr->nRows; ++i) {
+		for(j = 0; j < elr->nMaxRow; ++j) {
+			elr->col[i+j*elr->padding] = 0;
+			elr->val[i+j*elr->padding] = 0.0;
+		}
+	}
+	for(i = 0; i < elr->nRows; ++i) elr->rowLen[i] = 0;
 }
 
 
@@ -164,35 +165,35 @@ void resetELR( ELR_TYPE* elr ) {
 
 
 void elrColIdToFortran( ELR_TYPE* elr ) {
-  int i,j;
+	int i,j;
 
-  for(i = 0; i < elr->nRows; ++i) {
-    for(j = 0; j < elr->nMaxRow; ++j) {
-      elr->col[i+j*elr->padding] += 1;
-      if( elr->col[i+j*elr->padding] < 1 || elr->col[i+j*elr->padding] > elr->nRows ) {
-        fprintf(stderr, "error in elrColIdToFortran: index out of bounds\n");
-        exit(1);
-      }
-    }
-  }
+	for(i = 0; i < elr->nRows; ++i) {
+		for(j = 0; j < elr->nMaxRow; ++j) {
+			elr->col[i+j*elr->padding] += 1;
+			if( elr->col[i+j*elr->padding] < 1 || elr->col[i+j*elr->padding] > elr->nRows ) {
+				fprintf(stderr, "error in elrColIdToFortran: index out of bounds\n");
+				exit(1);
+			}
+		}
+	}
 }
 
 /* ########################################################################## */
 
 
 void elrColIdToC( ELR_TYPE* elr ) {
-  int i,j;
+	int i,j;
 
-  for(i = 0; i < elr->nRows; ++i) {
-    for(j = 0; j < elr->nMaxRow; ++j) {
-      elr->col[i+j*elr->padding] -= 1;
-      if( elr->col[i+j*elr->padding] < 0 || elr->col[i+j*elr->padding] > elr->nRows-1 ) {
-        fprintf(stderr, "error in elrColIdToC: index out of bounds: elr->col[%i][%i]=%i\n",
-        i,j,elr->col[i+j*elr->padding]);fflush(stderr);
-        exit(1);
-      }
-    }
-  }
+	for(i = 0; i < elr->nRows; ++i) {
+		for(j = 0; j < elr->nMaxRow; ++j) {
+			elr->col[i+j*elr->padding] -= 1;
+			if( elr->col[i+j*elr->padding] < 0 || elr->col[i+j*elr->padding] > elr->nRows-1 ) {
+				fprintf(stderr, "error in elrColIdToC: index out of bounds: elr->col[%i][%i]=%i\n",
+						i,j,elr->col[i+j*elr->padding]);fflush(stderr);
+				exit(1);
+			}
+		}
+	}
 }
 
 /* ########################################################################## */
@@ -200,33 +201,33 @@ void elrColIdToC( ELR_TYPE* elr ) {
 
 CUDA_ELR_TYPE* cudaELRInit( const ELR_TYPE* elr) {
 
-  /* allocate (but do not fill) memory for elr matrix on device */
+	/* allocate (but do not fill) memory for elr matrix on device */
 
 	int *col, *rowLen;
 	double *val;
-	
+
 	int me, ierr;
 
 	size_t colMemSize = (size_t) elr->padding * elr->nMaxRow * sizeof( int );
 	size_t valMemSize = (size_t) elr->padding * elr->nMaxRow * sizeof( double );
 	size_t rowMemSize = (size_t) elr->nRows * sizeof( int );
-	
+
 	/* allocate */
-	
+
 	IF_DEBUG(1) { 
 		ierr = MPI_Comm_rank(MPI_COMM_WORLD, &me);
 		printf("PE%i: CELRinitAlloc: in columns\t %lu MB\n", me, colMemSize/(1024*1024));	
 	}
 	col     = allocDeviceMemory( colMemSize );
-	
+
 	IF_DEBUG(1) printf("PE%i: CELRinitAlloc: in rows\t %lu MB\n",
-		me, valMemSize/(1024*1024));
+			me, valMemSize/(1024*1024));
 	val     = allocDeviceMemory( valMemSize );
-	
+
 	IF_DEBUG(1) printf("PE%i: CELRinitAlloc: in rLeng\t %lu MB\n",
-		me, rowMemSize/(1024*1024));
+			me, rowMemSize/(1024*1024));
 	rowLen  = allocDeviceMemory( rowMemSize );
-	
+
 	/* create host handle */
 	CUDA_ELR_TYPE* cuelr = (CUDA_ELR_TYPE*) allocateMemory( sizeof( CUDA_ELR_TYPE ), "cuda_elr");
 	cuelr->nRows   	= elr->nRows;
@@ -235,11 +236,11 @@ CUDA_ELR_TYPE* cudaELRInit( const ELR_TYPE* elr) {
 	cuelr->rowLen 	= rowLen;
 	cuelr->col		= col;
 	cuelr->val		= val;
-  IF_DEBUG(1) {
-    printf("PE%i: created cudaELR type from ELR with:\n nRows:\tcelr=%i\t(elr=%i)\n padding:\tcelr=%i\t(elr=%i)\nnMaxRow:\tcelr=%i\t(elr=%i)\n",
-		me, cuelr->nRows, elr->nRows, cuelr->padding, elr->padding,
-		cuelr->nMaxRow, elr->nMaxRow);
-  }
+	IF_DEBUG(1) {
+		printf("PE%i: created cudaELR type from ELR with:\n nRows:\tcelr=%i\t(elr=%i)\n padding:\tcelr=%i\t(elr=%i)\nnMaxRow:\tcelr=%i\t(elr=%i)\n",
+				me, cuelr->nRows, elr->nRows, cuelr->padding, elr->padding,
+				cuelr->nMaxRow, elr->nMaxRow);
+	}
 	return cuelr;
 }
 
@@ -249,13 +250,13 @@ CUDA_ELR_TYPE* cudaELRInit( const ELR_TYPE* elr) {
 
 void cudaCopyELRToDevice( CUDA_ELR_TYPE* celr,  const ELR_TYPE* elr ) {
 
-  /* copy col, val and rowLen from CPU elr format to device;
-   * celr must be allocated in advance (using cudaELRInit) */
+	/* copy col, val and rowLen from CPU elr format to device;
+	 * celr must be allocated in advance (using cudaELRInit) */
 
-  assert( celr->nRows == elr->nRows );
-  assert( celr->padding == elr->padding );
-  assert( celr->nMaxRow == elr->nMaxRow );
-	
+	assert( celr->nRows == elr->nRows );
+	assert( celr->padding == elr->padding );
+	assert( celr->nMaxRow == elr->nMaxRow );
+
 
 	int me, ierr;
 	ierr = MPI_Comm_rank(MPI_COMM_WORLD, &me);
@@ -265,17 +266,17 @@ void cudaCopyELRToDevice( CUDA_ELR_TYPE* celr,  const ELR_TYPE* elr ) {
 	size_t rowMemSize = (size_t) elr->nRows * sizeof( int );
 
 	IF_DEBUG(1) printf("PE%i: ELRtoDevice: col %lu \t(%lu MB)\n", me,
-		colMemSize, colMemSize/(1024*1024));
+			colMemSize, colMemSize/(1024*1024));
 	copyHostToDevice( celr->col, elr->col, colMemSize );
-	
+
 	IF_DEBUG(1) printf("PE%i: ELRtoDevice: val %lu \t(%lu MB)\n", me,
-		valMemSize, valMemSize/(1024*1024));
+			valMemSize, valMemSize/(1024*1024));
 	copyHostToDevice( celr->val, elr->val, valMemSize );
-	
+
 	IF_DEBUG(1) printf("PE%i: ELRtoDevice: row %lu \t(%lu MB)\n", me,
-		rowMemSize, rowMemSize/(1024*1024));
+			rowMemSize, rowMemSize/(1024*1024));
 	copyHostToDevice( celr->rowLen, elr->rowLen, rowMemSize );
-	
+
 }
 
 
@@ -283,33 +284,33 @@ void cudaCopyELRToDevice( CUDA_ELR_TYPE* celr,  const ELR_TYPE* elr ) {
 
 
 void cudaCopyELRBackToHost( ELR_TYPE* elr, const CUDA_ELR_TYPE* celr ) {
-  
-  /* copy col, val and rowLen from device celr to CPU;
-   * elr must be allocated in advance */
 
-  assert( celr->nRows == elr->nRows );
-  assert( celr->padding == elr->padding );
-  assert( celr->nMaxRow == elr->nMaxRow );
-  
+	/* copy col, val and rowLen from device celr to CPU;
+	 * elr must be allocated in advance */
+
+	assert( celr->nRows == elr->nRows );
+	assert( celr->padding == elr->padding );
+	assert( celr->nMaxRow == elr->nMaxRow );
+
 	int me, ierr;
 	ierr = MPI_Comm_rank(MPI_COMM_WORLD, &me);
 
 	size_t colMemSize = (size_t) elr->padding * elr->nMaxRow * sizeof( int );
 	size_t valMemSize = (size_t) elr->padding * elr->nMaxRow * sizeof( double );
 	size_t rowMemSize = (size_t) elr->nRows * sizeof( int );
-	
+
 	IF_DEBUG(1) printf("PE%i: ELRtoHost: col %lu \t(%lu MB)\n", me,
-		colMemSize, colMemSize/(1024*1024));
+			colMemSize, colMemSize/(1024*1024));
 	copyDeviceToHost( elr->col, celr->col, colMemSize );
-	
+
 	IF_DEBUG(1) printf("PE%i: ELRtoHost: val %lu \t(%lu MB)\n", me,
-		valMemSize, valMemSize/(1024*1024));
+			valMemSize, valMemSize/(1024*1024));
 	copyDeviceToHost( elr->val, celr->val, valMemSize );
-	
+
 	IF_DEBUG(1) printf("PE%i: ELRtoHost: row %lu \t(%lu MB)\n", me,
-		rowMemSize, rowMemSize/(1024*1024));
+			rowMemSize, rowMemSize/(1024*1024));
 	copyDeviceToHost( elr->rowLen, celr->rowLen, rowMemSize );
-	
+
 }
 
 
@@ -319,8 +320,8 @@ void cudaCopyELRBackToHost( ELR_TYPE* elr, const CUDA_ELR_TYPE* celr ) {
 void freeELRMatrix( ELR_TYPE* const elr ) {
 	if( elr ) {
 		freeHostMemory( elr->rowLen );
-    freeHostMemory( elr->col );
-    freeHostMemory( elr->val );
+		freeHostMemory( elr->col );
+		freeHostMemory( elr->val );
 		free( elr );
 	}
 }
--- thejob-gpu.csh
+++ thejob-gpu.csh
@@ -2,14 +2,14 @@
 
 #PBS -lnodes=4:ppn=16,walltime=01:00:00
 
-cd /home/woody/unrz/unrza337/mucosim/SpMVM/SpMVM/Hybrid
+cd /home/hpc/unrz/unrza317/proj/SpMVM/Hybrid
 
 module load intel64/11.1
 module load cuda
 
 #set pinlist = ( 0,1,2,3,4,5,6,7,8,9,10,11 0,1,2,3,4,5_6,7,8,9,10,11 0,1,2,3,4,5,6,7,8,9,10,11,15 0,1,2,3,4,5,15_6,7,8,9,10,11,20 0_1_2_3_4_5_6_7_8_9_10_11 0,12_1,13_2,14_3,15_4,16_5,17_6,18_7,19_8,20_9,21_10,22_11,23 )
-set pinlist = ( 0,1 0,1 0,1 ) 
-#set pinlist = ( 0,1_4,5 0,1_4,5 0,1_4,5 ) 
+set pinlist = ( 0,1 0,1 0,1 ) # 1 gpu pro knoten
+#set pinlist = ( (0,1)_(4,5) 0,1_4,5 0,1_4,5 ) # 2 gpus pro knoten
  
 set distscheme = ( NZE LNZ ROW ) # MPI distribution of matrix: equal_NZE equal_NZE_opt equal_row
 set mymasks    = ( 502 261640 262142 ) # kernel selector: full_only split_only all
@@ -21,7 +21,7 @@ set thismpi    = "/apps/rrze/bin/mpirun_rrze-intelmpd"
 set thispin    = "/apps/likwid/devel/bin/likwid-pin"
 set master     = `head -n 1 ${PBS_NODEFILE}`
 set skip       = "-s 0x3"
-set io_format  = "2" # 1 MPI-parallel / 2 binary / else ASCII
+set io_format  = "0" # 1 MPI-parallel / 2 binary / else ASCII
 set outerit    = "1"
 set datadir    = "./test"
 set wd_flag    = $distscheme[$work_dist]
