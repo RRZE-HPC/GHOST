3c36b08c43bd0b27188899f68f73a7af69431cdc
Moritz Kreutzer <moritz.kreutzer@rrze.fau.de>
2012-09-14 15:22:25 +0200

    stripped down minimal example

--- apps/minimal/main_minimal.c
+++ apps/minimal/main_minimal.c
@@ -22,21 +22,19 @@ int main( int argc, char* argv[] ) {
 	VECTOR_TYPE     *nodeLHS; // lhs vector per node
 	VECTOR_TYPE     *nodeRHS; // rhs vector node
 
-	int options = SPMVM_OPTION_NONE; // performan standard spmvm
-	int kernel = SPMVM_KERNEL_TASKMODE;
-
-	me      = SpMVM_init(argc,argv,options);    // basic initialization
-	lcrp    = SpMVM_createCRS(argv[1],NULL); // create CRS matrix from given matrix path
-	nodeRHS = SpMVM_createVector(lcrp,VECTOR_TYPE_RHS,rhsVal); // distribute RHS vector
-	nodeLHS = SpMVM_createVector(lcrp,VECTOR_TYPE_LHS,NULL); // distribute LHS vector
+	me      = SpMVM_init(argc,argv,SPMVM_OPTION_NONE); // basic initialization
+	lcrp    = SpMVM_createCRS(argv[1],NULL); // create CRS matrix 
+	nodeRHS = SpMVM_createVector(lcrp,VECTOR_TYPE_RHS,rhsVal); // RHS vec
+	nodeLHS = SpMVM_createVector(lcrp,VECTOR_TYPE_LHS,NULL);   // LHS vec (=0)
 
 	SpMVM_printEnvInfo();
 	SpMVM_printMatrixInfo(lcrp,strtok(basename(argv[1]),"_."),options);
 
-	time = SpMVM_solve(nodeLHS,lcrp,nodeRHS,kernel,nIter);
+	time = SpMVM_solve(nodeLHS,lcrp,nodeRHS,SPMVM_KERNEL_TASKMODE,nIter);
 
 	if (me == 0){
-		printf("%s kernel @ %7.2f GF/s\n",SpMVM_kernelName(kernel),2.0e-9*(double)nIter*(double)lcrp->nEnts/(time));
+		printf("%s kernel @ %7.2f GF/s\n",SpMVM_kernelName(kernel),
+				2.0e-9*(double)nIter*(double)lcrp->nEnts/(time));
 	}
 
 	SpMVM_freeVector( nodeLHS );
