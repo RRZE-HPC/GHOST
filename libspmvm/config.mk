3642a6ffb3d11bd40e98852498079449a00e25d8
Moritz Kreutzer <moritz.kreutzer@rrze.fau.de>
2012-09-14 14:46:32 +0200

    moved stuff to Makefile

--- libspmvm/config.mk
+++ libspmvm/config.mk
@@ -2,7 +2,7 @@ OPENCL=0
 LIKWID_MARKER=0
 LIKWID_MARKER_FINE=0
 DOUBLE=1
-COMPLEX=0
+COMPLEX=1
 DEBUG=0
 
 INSTDIR=~/app/libspmvm
@@ -13,40 +13,8 @@ LIKWID_LIB=/home/hpc/unrz/unrza317/app/likwid/lib
 
 
 CC	= mpicc
-CFLAGS  = -g -O3 -fno-alias -openmp -Wall -Werror-all -Wremarks -Wcheck -diag-disable 981 
+CFLAGS  = -O3 -fno-alias -openmp -Wall -Werror-all -Wremarks -Wcheck -diag-disable 981 
 
 FC	= ifort
 FFLAGS  = -O3 -nogen-interface -fno-alias -cpp -warn all -openmp 
 
-#------------------------------------------------------------------------------#
-#------------------------------------------------------------------------------#
-# DO NOT EDIT BELOW UNLESS YOU KNOW WHAT YOU ARE DOING                         #
-#------------------------------------------------------------------------------#
-#------------------------------------------------------------------------------#
-
-
-ifeq ($(OPENCL),1)
-MAKROS+= -DOPENCL
-PREFIX+= cl
-IPATH += -I${CUDA_INC}
-endif
-
-ifeq ($(LIKWID_MARKER),1)
-MAKROS+= -DLIKWID_MARKER
-IPATH += -I${LIKWID_INC}
-endif
-
-ifeq ($(LIKWID_MARKER_FINE),1)
-MAKROS+= -DLIKWID_MARKER_FINE
-IPATH += -I${LIKWID_INC}
-endif
-
-ifeq ($(DOUBLE),1)
-MAKROS+= -DDOUBLE
-endif
-
-ifeq ($(COMPLEX),1)
-MAKROS+= -DCOMPLEX
-endif
-
-MAKROS+= -DDEBUG=$(DEBUG)
