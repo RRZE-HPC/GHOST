053684604b01d78cd457ac3bd307d8e805991304
Moritz Kreutzer <di56xuq2@login05.(none)>
2012-09-18 14:32:09 +0200

    changed to SuperMUC paths

--- libspmvm/config.mk
+++ libspmvm/config.mk
@@ -3,12 +3,9 @@ COMPLEX=0              # 0 = real data             | 1 = complex data
 OPENCL=0               # 0 = standard CPU kernels  | 1 = OpenCL (GPU) kernels
 LIKWID_MARKER=0        # 0 = no calls              | 1 = Likwid Marker API calls
 LIKWID_MARKER_FINE=0   # 0 = few calls             | 1 = many calls 
-DEBUG=0                # 0 = no DEBUG output       | >0 = level of DEBUG output
+DEBUG=1                # 0 = no DEBUG output       | >0 = level of DEBUG output
 
-INSTDIR=/home/hpc/unrz/unrza317/app/libspmvm        # where to install LibSpMVM
-
-CL_INC=$(CUDA_HOME)/include   # where to find OpenCL headers (necessary if enabled)
-LIKWID_INC=/home/hpc/unrz/unrza317/app/likwid/include
+INSTDIR=/home/hpc/h021z/di56xuq2/app/libspmvm        # where to install LibSpMVM
 
 CC	= mpicc           # the C compiler
 CFLAGS  = -O3 -fno-alias -openmp -Wall -Werror-all -Wremarks -Wcheck -diag-disable 981 
