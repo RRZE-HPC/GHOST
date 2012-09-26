03a0711dfa373f34dcde27b2aac308166769248b
Moritz Kreutzer <moritz.kreutzer@fau.de>
2012-09-26 02:59:08 +0200

    simplified

--- test/test.pl
+++ test/test.pl
@@ -17,7 +17,7 @@ print "=== STAGE 1: BUILD ===\n";
 print "  Preparation... ";
 dircopy($srcpath,$libpath) or die "$!";
 chdir("./libspmvm");
-move('config.mk','config.mk.orig');
+copy('config.mk','config.mk.orig');
 print "succeeded.\n";
 
 my $nopt = $#buildoptions+1; # number of build options
@@ -29,21 +29,18 @@ for ($optperm=0; $optperm<$maxoptperm; $optperm++)
 {
 	my @bitmask = split(//,sprintf("%0".$nopt."b", $optperm));
 
-	open (IN,"config.mk.orig") or die "$!";
-	open (OUT,">config.mk") or die "$!";
+	copy('config.mk.orig','config.mk');   # restore original config.mk
+	open (OUT,">>config.mk") or die "$!"; # append new options to file (overrides)
 
-	while ($line = <IN>)
+	for ($opt=0; $opt<$nopt; $opt++)
 	{
-		for ($opt=0; $opt<$nopt; $opt++)
-		{
-			my $curopt = $buildoptions[$opt];
-			$line =~ s/$curopt=[01]/$curopt=$bitmask[$opt]/g;
-		}
-		print OUT $line;
+		print OUT $buildoptions[$opt]."=".$bitmask[$opt]."\n";
 	}
 
-	close(IN);
 	close(OUT);
+
+# make
+		$succeeded++;
 
 }
 print "  $succeeded/$maxoptperm succeded\n";
@@ -52,6 +49,6 @@ print "  $succeeded/$maxoptperm succeded\n";
 
 chdir("..");
 
-print "=== STAGE 2: CORRECTNESS ===\n"
-#remove_tree($libpath,$instpath);
+print "=== STAGE 2: CORRECTNESS ===\n";
+remove_tree($libpath,$instpath);
 
