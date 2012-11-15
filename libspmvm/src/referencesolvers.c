d70b90773d241ecc7858e7779f1508ec8a553700
Moritz Kreutzer <moritz.kreutzer@rrze.fau.de>
2012-11-15 09:51:21 +0100

    Fortran to C

---
+++ libspmvm/src/referencesolvers.c
@@ -0,0 +1,22 @@
+#include "referencesolvers.h"
+
+void SpMVM_referenceKernel(mat_data_t *res, mat_nnz_t *col, mat_idx_t *rpt, mat_data_t *val, mat_data_t *rhs, mat_idx_t nrows, int spmvmOptions)
+{
+
+	mat_idx_t i, j;
+	mat_data_t hlp1;
+
+#pragma omp	parallel for schedule(runtime) private (hlp1, j)
+	for (i=0; i<nrows; i++){
+		hlp1 = 0.0;
+		for (j=rpt[i]; j<rpt[i+1]; j++){
+			hlp1 = hlp1 + val[j] * rhs[col[j]]; 
+		}
+		if (spmvmOptions & SPMVM_OPTION_AXPY) 
+			res[i] += hlp1;
+		else
+			res[i] = hlp1;
+	}
+
+
+}	
