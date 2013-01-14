8b51903dc39a8f7ed0027de19646e7d80e6814d0
Moritz Kreutzer <moritz.kreutzer@rrze.fau.de>
2013-01-14 11:15:17 +0100

    removed subfolder avx/sse/mic (intrinsics kernels are in plugins)

--- ghost/config.mk
+++ ghost/config.mk
@@ -6,7 +6,7 @@ LONGIDX=0       # 0/1 = indices are 32/64 bit integers
 MPI=1           # 0 = no MPI support        | 1 = with MPI support
 MIC=0           # 0 = no MIC kernels        | 1 = build MIC kernels
 AVX=0           # 0 = no AVX kernels        | 1 = build AVX kernels
-SSE=0           # 0 = no SSE kernels        | 1 = build SSE kernels
+SSE=1           # 0 = no SSE kernels        | 1 = build SSE kernels
 OPENCL=1        # 0 = standard CPU kernels  | 1 = OpenCL (GPU) kernels
 LIKWID=0        # 0 = no instrumentation    | 1 = Likwid Marker API instrumentation
 DEBUG=0         # 0 = no DEBUG output       | >0 = level of DEBUG output
