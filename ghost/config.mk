e5886ae5bc575e46c1c019c18e33a6f0d5bb2308
Moritz Kreutzer <moritz.kreutzer@rrze.fau.de>
2013-01-15 14:26:08 +0100

    updated config.mk

--- ghost/config.mk
+++ ghost/config.mk
@@ -7,7 +7,7 @@ MPI=0           # 0 = no MPI support        | 1 = with MPI support
 MIC=0           # 0 = no MIC kernels        | 1 = build MIC kernels
 AVX=0           # 0 = no AVX kernels        | 1 = build AVX kernels
 SSE=0           # 0 = no SSE kernels        | 1 = build SSE kernels
-OPENCL=0        # 0 = standard CPU kernels  | 1 = OpenCL (GPU) kernels
+OPENCL=0        # 0 = no OpenCL kernels     | 1 = build OpenCL kernels
 LIKWID=0        # 0 = no instrumentation    | 1 = Likwid Marker API instrumentation
 DEBUG=0         # 0 = no DEBUG output       | >0 = level of DEBUG output
 
