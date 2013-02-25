aa2cd8575c97a5f62888fae127fb0d31dfc858aa
Moritz Kreutzer <moritz.kreutzer@rrze.fau.de>
2013-02-25 17:06:08 +0100

    added Fortran stuff to build system (only ICC at the moment, app/fminimal still missing)

--- ghost/include_ICC.mk
+++ ghost/include_ICC.mk
@@ -2,9 +2,11 @@ ifeq ($(strip $(MPI)),1)
 CC = mpicc
 else
 CC = icc
+FC = ifort
 endif
 
-CFLAGS  = -openmp -fPIC -std=c99 
+CFLAGS  = -openmp -fPIC -std=c99
+FFLAGS  = -fPIC -module $(MODDIR)
 SHAREDFLAG = -shared
 
 LIBS = -limf -lm
@@ -24,6 +26,8 @@ endif
 
 ifneq ($(strip $(DEBUG)),0)
 CFLAGS += -g -O0 #-check-pointers=rw -check-pointers-dangling=all -rdynamic
+FFLAGS += -g -O0
 else
 CFLAGS += -O3 -Wall -Werror-all -Wremarks -Wcheck -diag-disable 981
+FFLAGS += -O3 -Wall -Werror-all -Wremarks -Wchec
 endif
