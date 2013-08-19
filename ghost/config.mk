0dc1ad754ffdcbf6f6ff08c0359824374b4c106b
Moritz Kreutzer <moritz.kreutzer@rrze.fau.de>
2013-08-19 13:57:45 +0200

    moved stripping of config variables to config.mk

--- ghost/config.mk
+++ ghost/config.mk
@@ -2,7 +2,7 @@ COMPILER=ICC    # one of: ICC, GCC, XLC
 LIBTYPE=shared  # static or shared (default)
 LONGIDX=0       # 0/1 = indices are 32/64 bit integers
 FORTRAN=0       # 0/1 = build without/with Fortran interface
-OPENMP=0        # 0/1 = build without/with OpenMP
+OPENMP=1        # 0/1 = build without/with OpenMP
 MPI=0           # 0 = no MPI support        | 1 = with MPI support
 VSX=0           # 0 = no VSX kernels        | 1 = build VSX kernels
 MIC=0           # 0 = no MIC | 1 = compile for MIC | 2 = w/ intrinsics
@@ -12,7 +12,27 @@ OPENCL=0        # 0 = no OpenCL kernels     | 1 = build OpenCL kernels
 CUDA=0          # 0 = no CUDA kernels       | 1 = build CUDA kernels
 LIKWID=0        # 0 = no instrumentation    | 1 = Likwid Marker API instrumentation
 ITAC=0          # 0 = no instrumentation    | 1 = VT instrumentation
-DEBUG=-1        # -1 = only debug compilation | >0 = level of DEBUG output
+DEBUG=0        # -1 = only debug compilation | >0 = level of DEBUG output
 
 GHOSTPATH=$(HOME)/local      # where to install ghost
 LIKWIDPATH=/apps/likwid/likwid-3.1beta/     # path to LIKWID installation (if enabled)
+
+
+##### DO NOT EDIT BELOW
+
+COMPILER:=$(strip $(COMPILER))
+LIBTYPE:=$(strip $(LIBTYPE))
+LONGIDX:=$(strip $(LONGIDX))
+OPENMP:=$(strip $(OPENMP))
+FORTRAN:=$(strip $(FORTRAN))
+MPI:=$(strip $(MPI))
+VSX:=$(strip $(VSX))
+MIC:=$(strip $(MIC))
+AVX:=$(strip $(AVX))
+SSE:=$(strip $(SSE))
+OPENCL:=$(strip $(OPENCL))
+CUDA:=$(strip $(CUDA))
+LIKWID:=$(strip $(LIKWID))
+DEBUG:=$(strip $(DEBUG))
+GHOSTPATH:=$(strip $(GHOSTPATH))
+LIKWIDPATH:=$(strip $(LIKWIDPATH))
