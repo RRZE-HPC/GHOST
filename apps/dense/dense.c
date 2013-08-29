5fcaea7caf54782abea370f7d965db9a1daf340f
Moritz Kreutzer <moritz.kreutzer@rrze.fau.de>
2013-08-29 09:55:30 +0200

    cleanup

--- apps/dense/dense.c
+++ apps/dense/dense.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <ghost.h>
 #include <ghost_util.h>
+#include <ghost_vec.h>
 
-#include <omp.h>
 #include <unistd.h>
 
 GHOST_REGISTER_DT_D(vecdt)
@@ -11,39 +11,27 @@ GHOST_REGISTER_DT_D(matdt)
 int main(int argc, char* argv[]) 
 {
 	int nv = 2;
-	vecdt_t zero = 0.;
 	vecdt_t one = 1.;
 	vecdt_t dotpr[nv];
 
-//	ghost_mtraits_t mtraits = GHOST_MTRAITS_INIT(.format = GHOST_SPM_FORMAT_CRS,.datatype = matdt);
 	ghost_vtraits_t dmtraits = GHOST_VTRAITS_INIT(.flags = GHOST_VEC_LHS, .nvecs=nv, .datatype=vecdt);
 
 	ghost_context_t *ctx;
 	ghost_vec_t *dm1, *dm2, *dm3;
-//	ghost_mat_t *mat;
 
 	ghost_init(argc,argv);
 
-	if (argc == 1) {
-		ctx = ghost_createContext(8,8,GHOST_CONTEXT_DEFAULT,NULL);
-	} else {
-		printf("!!!\n");
-		ghost_matfile_header_t fileheader;
-		ghost_readMatFileHeader(argv[1],&fileheader);
-		ctx = ghost_createContext(fileheader.nrows,fileheader.ncols,GHOST_CONTEXT_WORKDIST_NZE,argv[1]);
-	}
-//	mat = ghost_createMatrix(&mtraits,1); // FIXME we need the matrix in order to have the distribution. fix this bug!
-	dm1 = ghost_createVector(&dmtraits);
-	dm2 = ghost_createVector(&dmtraits);
+	ctx = ghost_createContext(8,8,GHOST_CONTEXT_DEFAULT,NULL,MPI_COMM_WORLD);
+	dm1 = ghost_createVector(ctx,&dmtraits);
+	dm2 = ghost_createVector(ctx,&dmtraits);
 
-//	mat->fromFile(mat,ctx,argv[1]);
-	dm1->fromScalar(dm1,ctx,&one);
-	dm2->fromScalar(dm2,ctx,&one);
-	
+	dm1->fromScalar(dm1,&one);
+	dm2->fromScalar(dm2,&one);
+
 	dm1->print(dm1);
 
 	dm2->print(dm2);
-	
+
 	for (int i=0; i<nv; i++)
 		dotpr[i] = 0.;
 
@@ -54,8 +42,8 @@ int main(int argc, char* argv[])
 	printf("\n");
 
 	ghost_vtraits_t dm3traits = GHOST_VTRAITS_INIT(.flags = GHOST_VEC_DEFAULT, .nrows = dm1->traits->nvecs, .nvecs = dm2->traits->nvecs, .datatype=vecdt);
-	dm3 = ghost_createVector(&dm3traits);
-	dm3->fromScalar(dm3,NULL,&one);
+	dm3 = ghost_createVector(ctx,&dm3traits);
+	dm3->fromScalar(dm3,&one);
 
 	vecdt_t alpha = 1., beta = 1.;
 	ghost_gemm("T",dm1,dm2,dm3,&alpha,&beta,GHOST_GEMM_ALL_REDUCE);
@@ -65,8 +53,8 @@ int main(int argc, char* argv[])
 	printf("\n##### write\n");
 	dm2->print(dm2);
 	printf("##### read\n");
-	ghost_vecToFile(dm2,"foo.vec",ctx);
-	ghost_vecFromFile(dm2,"foo.vec",ctx);
+	ghost_vecToFile(dm2,"foo.vec");
+	ghost_vecFromFile(dm2,"foo.vec");
 	dm2->print(dm2);
 	printf("#####\n");
 
