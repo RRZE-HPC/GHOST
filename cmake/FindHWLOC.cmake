b0e8ec7a50befd2242ad0ceba3b2ebf401d3926f
Moritz Kreutzer <moritz.kreutzer@rrze.fau.de>
2013-10-30 09:14:33 +0100

    bugfix: cmake did not abort when hwloc was not found

--- cmake/FindHWLOC.cmake
+++ cmake/FindHWLOC.cmake
@@ -1,4 +1,6 @@
 include(LibFindMacros)
 
-libfind_pkg_check_modules(HWLOC hwloc)
+libfind_pkg_check_modules(HWLOC_PKGCONF hwloc)
+find_path(HWLOC_INCLUDE_DIR NAMES hwloc.h PATHS ${HWLOC_PKGCONF_INCLUDE_DIRS})
+set(HWLOC_PROCESS_INCLUDES HWLOC_INCLUDE_DIR)
 libfind_process(HWLOC)
