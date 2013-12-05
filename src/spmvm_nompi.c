73af7ae208b9e0bb02bb05689fd8149fd576aefb
Moritz Kreutzer <moritz.kreutzer@rrze.fau.de>
2013-12-05 15:14:23 +0100

    clean up and added GHOST instrumentation

--- src/spmvm_nompi.c
+++ src/spmvm_nompi.c
@@ -1,21 +1,15 @@
-#include "ghost_util.h"
-#include <stdio.h>
-
-#ifdef LIKWID_PERFMON
-#include <likwid.h>
-#endif
+#include <ghost_config.h>
+#include <ghost_types.h>
+#include <ghost_util.h>
 
 void ghost_solver_nompi(ghost_context_t *context, ghost_vec_t* res, ghost_mat_t* mat, ghost_vec_t* invec, int spmvmOptions)
 {
     if (context == NULL)
         return;
-#ifdef LIKWID_PERFMON
-#pragma omp parallel
-    LIKWID_MARKER_START("full SpMVM");
-#endif
+
+    GHOST_INSTR_START(spmvm_nompi);
+    
     mat->kernel(mat,res,invec,spmvmOptions);
-#ifdef LIKWID_PERFMON
-#pragma omp parallel
-    LIKWID_MARKER_STOP("full SpMVM");
-#endif
+    
+    GHOST_INSTR_STOP(spmvm_nompi);
 }
