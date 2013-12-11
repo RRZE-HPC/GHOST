b35e00d0e3daf505f76abac0377658cb46f3e5de
Moritz Kreutzer <moritz.kreutzer@rrze.fau.de>
2013-12-11 08:40:09 +0100

    ghost_complex no longer inherits from std::complex. this solves many problems

--- include/ghost_complex.h
+++ include/ghost_complex.h
@@ -1,34 +1,56 @@
 #include <cmath>
+#include <iostream>
 #include <complex>
 
 template <typename T> 
-struct ghost_complex : public std::complex<T> 
+class ghost_complex
 {
-        ghost_complex() : std::complex<T>(0.,0.) {}
-        ghost_complex(T a) : std::complex<T>(a) {}
-        ghost_complex(T a, T b) : std::complex<T>(a,b) {}
-        ghost_complex(const std::complex<T> &a) : std::complex<T>(a) {}
-        ghost_complex(const ghost_complex<float> &a) : std::complex<T>(a) {}
-        operator float() const;
+    private:
+        T re, im;
+    public:
+        ghost_complex() {re = 0.; im = 0.;}
+        ghost_complex(T a)  {re = a; im = 0.;}
+        ghost_complex(T a, T b) { re = a; im = b;}
+        //ghost_complex(const std::complex<T> &a) : std::complex<T>(a) {re = a.real();}
+        //ghost_complex(const ghost_complex<float> &a) : std::complex<T>(a) {re = a.re;}
+        operator T() const;
+        ghost_complex<T> operator-(const ghost_complex<T>&) const;
         ghost_complex<T> operator+(const ghost_complex<T>&) const;
+        ghost_complex<T>& operator+=(const ghost_complex<T>&);
         ghost_complex<T> operator*(const ghost_complex<T>&) const;
+        ghost_complex<T>& operator*=(const ghost_complex<T>&);
 };
 
 template <typename T>
-ghost_complex<T>::operator float() const {
-    return (float)(std::real(*this));
+ghost_complex<T>::operator T() const {
+    return re;
+}
+
+template <typename T>
+ghost_complex<T>& ghost_complex<T>::operator +=(const ghost_complex<T>& c) {
+    *this = *this + c;
+    return *this;
+}
+
+template <typename T>
+ghost_complex<T>& ghost_complex<T>::operator *=(const ghost_complex<T>& c) {
+    *this = *this * c;
+    return *this;
 }
 
 template <typename T>
 ghost_complex<T> ghost_complex<T>::operator +(const ghost_complex<T>& c) const {
-    return ghost_complex<T>(std::real(*this) + std::real(c), std::imag(*this) + std::imag(c));
+    return ghost_complex<T>(this->re + c.re, this->im + c.im);
+}
+
+template <typename T>
+ghost_complex<T> ghost_complex<T>::operator -(const ghost_complex<T>& c) const {
+    return ghost_complex<T>(this->re - c.re, this->im - c.im);
 }
 
 template <typename T>
 ghost_complex<T> ghost_complex<T>::operator *(const ghost_complex<T>& c) const {
-    return ghost_complex<T>(
-            std::real(std::real(*this)*std::real(c) - std::imag(*this)*std::imag(c)), 
-            std::real(std::real(*this)*std::imag(c) + std::imag(*this)*std::real(c)));
+    return ghost_complex<T>(this->re*c.re - this->im*c.im, this->re*c.im + this->im*c.re);
 }
 
 
