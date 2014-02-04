40727f97faaa7c1feb79a60dd81c55d93aac9f98
Moritz Kreutzer <moritz.kreutzer@rrze.fau.de>
2014-02-04 15:06:10 +0100

    added cplusplus guard

--- include/ghost/machine.h
+++ include/ghost/machine.h
@@ -5,6 +5,10 @@
 #include <hwloc.h>
 #include "ghost/error.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 ghost_error_t ghost_initTopology();
 ghost_error_t ghost_getTopology(hwloc_topology_t *topo);
 ghost_error_t ghost_getSizeOfLLC(uint64_t *size);
@@ -13,5 +17,9 @@ ghost_error_t ghost_getNumberOfPhysicalCores(int *nCores);
 ghost_error_t ghost_getSMTlevel(int *nLevels);
 ghost_error_t ghost_getNumberOfHwThreads(int *nThreads);
 ghost_error_t ghost_getNumberOfNumaNodes(int *nNodes);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
