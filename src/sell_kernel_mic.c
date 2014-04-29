e3c497433b2df1472fc5f929b9402d21b421162c
Moritz Kreutzer <moritz.kreutzer@rrze.fau.de>
2014-04-29 16:13:05 +0200

    first version using GHOST preprocessor

--- src/sell_kernel_mic.c
+++ src/sell_kernel_mic.c
@@ -7,52 +7,35 @@
 #include "ghost/omp.h"
 #include <immintrin.h>
 
-ghost_error_t dd_SELL_kernel_MIC_16(ghost_sparsemat_t *mat, ghost_densemat_t* res, ghost_densemat_t* invec, ghost_spmv_flags_t spmvmOptions,va_list argp)
+#GHOST_FUNC_BEGIN#CHUNKHEIGHT=16,32
+ghost_error_t dd_SELL_kernel_MIC_CHUNKHEIGHT(ghost_sparsemat_t *mat, ghost_densemat_t* res, ghost_densemat_t* invec, ghost_spmv_flags_t spmvmOptions,va_list argp)
 {
     UNUSED(argp);
 #ifdef GHOST_HAVE_MIC
+    INFO_LOG("in MIC kernel with chunkheight %d",CHUNKHEIGHT);
     ghost_idx_t c,j;
     ghost_nnz_t offs;
     double *mval = (double *)SELL(mat)->val;
     double *lval = (double *)res->val[0];
     double *rval = (double *)invec->val[0];
-    __m512d tmp1;
-    __m512d tmp2;
     __m512d val;
     __m512d rhs;
     __m512i idx;
 
-#pragma omp parallel for schedule(runtime) private(j,tmp1,tmp2,idx,val,rhs,offs)
-    for (c=0; c<mat->nrowsPadded>>4; c++) 
+#pragma omp parallel for schedule(runtime) private(j,idx,val,rhs,offs)
+    for (c=0; c<mat->nrowsPadded/CHUNKHEIGHT; c++) 
     { // loop over chunks
-        tmp1 = _mm512_setzero_pd(); // tmp1 = 0
-        tmp2 = _mm512_setzero_pd(); // tmp2 = 0
+        #GHOST_UNROLL#__m512d tmp@ = _mm512_setzero_pd();#CHUNKHEIGHT/8
         offs = SELL(mat)->chunkStart[c];
 
-        for (j=0; j<(SELL(mat)->chunkStart[c+1]-SELL(mat)->chunkStart[c])>>4; j++) 
+        for (j=0; j<(SELL(mat)->chunkStart[c+1]-SELL(mat)->chunkStart[c])/CHUNKHEIGHT; j++) 
         { // loop inside chunk
-            val = _mm512_load_pd(&mval[offs]);
-            idx = _mm512_load_epi32(&SELL(mat)->col[offs]);
-            rhs = _mm512_i32logather_pd(idx,rval,8);
-            //            rhs = _mm512_extload_pd(&rval[SELL(mat)->col[offs]],_MM_UPCONV_PD_NONE,_MM_BROADCAST_1X8,_MM_HINT_NONE);
-            tmp1 = _mm512_add_pd(tmp1,_mm512_mul_pd(val,rhs));
-
-            offs += 8;
-
-            val = _mm512_load_pd(&mval[offs]);
-            idx = _mm512_permute4f128_epi32(idx,_MM_PERM_BADC);
-            rhs = _mm512_i32logather_pd(idx,rval,8);
-            //            rhs = _mm512_extload_pd(&rval[SELL(mat)->col[offs]],_MM_UPCONV_PD_NONE,_MM_BROADCAST_1X8,_MM_HINT_NONE);
-            tmp2 = _mm512_add_pd(tmp2,_mm512_mul_pd(val,rhs));
-
-            offs += 8;
+            #GHOST_UNROLL#val = _mm512_load_pd(&mval[offs]);idx = _mm512_load_epi32(&SELL(mat)->col[offs]);rhs = _mm512_i32logather_pd(idx,rval,8);tmp2*@ = _mm512_add_pd(tmp2*@,_mm512_mul_pd(val,rhs));offs += 8;val = _mm512_load_pd(&mval[offs]);idx = _mm512_permute4f128_epi32(idx,_MM_PERM_BADC);rhs = _mm512_i32logather_pd(idx,rval,8);tmp2*@+1 = _mm512_add_pd(tmp2*@+1,_mm512_mul_pd(val,rhs));#CHUNKHEIGHT/16
         }
         if (spmvmOptions & GHOST_SPMV_AXPY) {
-            _mm512_store_pd(&lval[c*SELL(mat)->chunkHeight],_mm512_add_pd(tmp1,_mm512_load_pd(&lval[c*SELL(mat)->chunkHeight])));
-            _mm512_store_pd(&lval[c*SELL(mat)->chunkHeight+8],_mm512_add_pd(tmp2,_mm512_load_pd(&lval[c*SELL(mat)->chunkHeight+8])));
+            #GHOST_UNROLL#_mm512_store_pd(&lval[c*SELL(mat)->chunkHeight+8*@],_mm512_add_pd(tmp@,_mm512_load_pd(&lval[c*SELL(mat)->chunkHeight])));#CHUNKHEIGHT/8
         } else {
-            _mm512_storenrngo_pd(&lval[c*SELL(mat)->chunkHeight],tmp1);
-            _mm512_storenrngo_pd(&lval[c*SELL(mat)->chunkHeight+8],tmp2);
+            #GHOST_UNROLL#_mm512_storenrngo_pd(&lval[c*SELL(mat)->chunkHeight+8*@],tmp@);#CHUNKHEIGHT/8
         }
     }
 #else 
@@ -63,8 +46,9 @@ ghost_error_t dd_SELL_kernel_MIC_16(ghost_sparsemat_t *mat, ghost_densemat_t* re
 #endif
     return GHOST_SUCCESS;
 }
+#GHOST_FUNC_END
 
-ghost_error_t dd_SELL_kernel_MIC_32(ghost_sparsemat_t *mat, ghost_densemat_t* res, ghost_densemat_t* invec, ghost_spmv_flags_t spmvmOptions,va_list argp)
+/*ghost_error_t dd_SELL_kernel_MIC_32(ghost_sparsemat_t *mat, ghost_densemat_t* res, ghost_densemat_t* invec, ghost_spmv_flags_t spmvmOptions,va_list argp)
 {
     UNUSED(argp);
 #ifdef GHOST_HAVE_MIC
@@ -92,10 +76,6 @@ ghost_error_t dd_SELL_kernel_MIC_32(ghost_sparsemat_t *mat, ghost_densemat_t* re
 
         for (j=0; j<(SELL(mat)->chunkStart[c+1]-SELL(mat)->chunkStart[c])>>5; j++) 
         { // loop inside chunk
-            //        _mm_prefetch(&((const char*)mval)[offs+512], _MM_HINT_T1);
-            //        _mm_prefetch(&((const char*)SELL(mat)->col)[offs+512], _MM_HINT_T1);
-            //        _mm_prefetch(&((const char*)mval)[offs+100000], _MM_HINT_NTA);
-            //        _mm_prefetch(&((const char *)SELL(mat)->col)[offs+500000], _MM_HINT_T0);
 
             val = _mm512_load_pd(&mval[offs]);
             idx = _mm512_load_epi32(&SELL(mat)->col[offs]);
@@ -144,4 +124,4 @@ ghost_error_t dd_SELL_kernel_MIC_32(ghost_sparsemat_t *mat, ghost_densemat_t* re
     UNUSED(spmvmOptions);
 #endif
     return GHOST_SUCCESS;
-}
+}*/
