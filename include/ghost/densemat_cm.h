bea75f67d5066019eb3e98c2b0bb8f9f95052bf2
Moritz Kreutzer <moritz.kreutzer@rrze.fau.de>
2014-05-22 14:56:52 +0200

    pulled function call out of loop, re #104

--- include/ghost/densemat_cm.h
+++ include/ghost/densemat_cm.h
@@ -40,9 +40,10 @@
 #define ITER_COMPACT_ROWS_BEGIN(vec,row,rowidx)\
     rowidx = 0;\
     row = 0;\
+    int offs = hwloc_bitmap_first(vec->ldmask);\
     _Pragma("omp for schedule(runtime)")\
     for (rowidx=0; rowidx<vec->traits.nrows; rowidx++) {\
-        row = hwloc_bitmap_first(vec->ldmask)+rowidx;\
+        row = offs+rowidx;\
 
 #define ITER_COMPACT_ROWS_END()\
     }
@@ -98,10 +99,12 @@
     rowidx = 0;\
     row1 = 0;\
     row2 = 0;\
+    const int offs1 = hwloc_bitmap_first(vec1->ldmask);\
+    const int offs2 = hwloc_bitmap_first(vec1->ldmask);\
     _Pragma("omp for schedule(runtime)")\
     for (rowidx=0; rowidx<vec->traits.nrows; rowidx++) {\
-        row1 = hwloc_bitmap_first(vec1->ldmask)+rowidx;\
-        row2 = hwloc_bitmap_first(vec2->ldmask)+rowidx;
+        row1 = offs1+rowidx;\
+        row2 = offs2+rowidx;
 
 #define ITER2_COMPACT_ROWS_END()\
     }
