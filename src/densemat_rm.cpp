2d40f7f5e9a0e2fc5f4105c6b285b1e76b597758
Moritz Kreutzer <moritz.kreutzer@rrze.fau.de>
2014-05-23 10:08:42 +0200

    typo

--- src/densemat_rm.cpp
+++ src/densemat_rm.cpp
@@ -180,7 +180,7 @@ static ghost_error_t ghost_densemat_rm_vaxpby_tmpl(ghost_densemat_t *vec, ghost_
     v_t *b = (v_t *)b_;
     
     if (!hwloc_bitmap_isfull(vec->ldmask) || !hwloc_bitmap_isfull(vec2->ldmask)) {
-        WARNING_LOG("Potentially slow VAXPY operation because some rows are masked out!");
+        WARNING_LOG("Potentially slow VAXPBY operation because some rows are masked out!");
         ITER2_BEGIN_RM(vec,vec2,col1,col2,row,colidx)
             *(v_t *)VECVAL_RM(vec,vec->val,row,col1) = *(v_t *)VECVAL_RM(vec2,vec2->val,row,col2) * s[colidx] + 
                 *(v_t *)VECVAL_RM(vec,vec->val,row,col1) * b[colidx];
