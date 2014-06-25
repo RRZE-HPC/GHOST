bb99f752004d3d46d4d23b7c5e7025aa3745b431
Moritz Kreutzer <moritz.kreutzer@rrze.fau.de>
2014-06-25 14:46:21 +0200

    bugfix: C instead of T in the complex case. also: added datatype check

--- src/tsmttsm.c
+++ src/tsmttsm.c
@@ -3,13 +3,22 @@
     
 ghost_error_t ghost_tsmttsm(ghost_densemat_t *x, ghost_densemat_t *v, ghost_densemat_t *w, void *alpha, void *beta)
 {
+    if (x->traits.datatype != v->traits.datatype || x->traits.datatype != w->traits.datatype) {
+        ERROR_LOG("Mixed data types not supported!");
+        return GHOST_ERR_INVALID_ARG;
+    }
 
     ghost_tsmttsm_parameters_t par = {.dt = x->traits.datatype, .blocksz = x->traits.ncols};
     tsmttsm_kernel kernel = ghost_tsmttsm_kernel(par);
 
     if (!kernel) {
         INFO_LOG("Could not find TSMTTSM kernel with %d %d. Fallback to GEMM",par.dt,par.blocksz);
-        return ghost_gemm(x,v,"T",w,"N",alpha,beta,GHOST_GEMM_ALL_REDUCE);
+        
+        if (x->traits.datatype & GHOST_DT_REAL) {
+            return ghost_gemm(x,v,"T",w,"N",alpha,beta,GHOST_GEMM_ALL_REDUCE);
+        } else {
+            return ghost_gemm(x,v,"C",w,"N",alpha,beta,GHOST_GEMM_ALL_REDUCE);
+        }
     }
 
 
