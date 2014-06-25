59c77b80a3a881a35a4e4df6ebbe2ba5a83b68f2
Moritz Kreutzer <moritz.kreutzer@rrze.fau.de>
2014-06-25 13:50:07 +0200

    better handling of non-standard densemats and bugfix with wrong beta value to gemm()

--- src/tsmm.c
+++ src/tsmm.c
@@ -7,10 +7,19 @@ ghost_error_t ghost_tsmm(ghost_densemat_t *x, ghost_densemat_t *v, ghost_densema
     ghost_tsmm_parameters_t par = {.dt = x->traits.datatype, .blocksz1 = x->traits.ncols, .blocksz2 = v->traits.ncols};
     tsmm_kernel kernel = ghost_tsmm_kernel(par);
 
-    if (!kernel) {
+    if (!kernel || (x->traits.flags & GHOST_DENSEMAT_SCATTERED)|| (v->traits.flags & GHOST_DENSEMAT_SCATTERED) || (w->traits.flags & GHOST_DENSEMAT_SCATTERED) || !hwloc_bitmap_isfull(x->ldmask) || !hwloc_bitmap_isfull(v->ldmask) || !hwloc_bitmap_isfull(w->ldmask)) {
         INFO_LOG("Could not find TSMM kernel with %d %d %d. Fallback to GEMM.",par.dt,par.blocksz1,par.blocksz2);
-        char zero[x->elSize];
-        return ghost_gemm(x,v,"N",w,"N",alpha,zero,GHOST_GEMM_NO_REDUCE);
+        if (x->traits.datatype != v->traits.datatype || x->traits.datatype != w->traits.datatype) {
+            ERROR_LOG("Mixed data types not supported!");
+            return GHOST_ERR_INVALID_ARG;
+        }
+        if (x->traits.datatype & GHOST_DT_DOUBLE) {
+            complex double one = 1.+I*1.;
+            return ghost_gemm(x,v,"N",w,"N",alpha,&one,GHOST_GEMM_NO_REDUCE);
+        } else {
+            complex float one = 1.+I*1.;
+            return ghost_gemm(x,v,"N",w,"N",alpha,&one,GHOST_GEMM_NO_REDUCE);
+        }
     }
 
 
