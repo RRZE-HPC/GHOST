231c869312dfa23ccd34bed1db67bd1c54a7d0d2
Moritz Kreutzer <moritz.kreutzer@rrze.fau.de>
2014-08-14 15:48:58 +0200

    more documentation

--- include/ghost/tsmm.h
+++ include/ghost/tsmm.h
@@ -12,11 +12,23 @@
 
 typedef struct
 {
+    /**
+     * @brief The data type of the densemats.
+     */
     ghost_datatype_t dt;
+    /**
+     * @brief The first configured block size K.
+     */
     int blocksz1;
+    /**
+     * @brief The second configure block size M.
+     */
     int blocksz2;
 } ghost_tsmm_parameters_t;
 
+/**
+ * @brief A tsmm kernel function. 
+ */
 typedef ghost_error_t (*tsmm_kernel)(ghost_densemat_t *, ghost_densemat_t *, ghost_densemat_t *, void *, void *);
 
 
@@ -43,6 +55,7 @@ extern "C" {
      * w is MxK, redundant, col-major.
      *
      * x is NxK, distributed, row-major.
+     *
      * M<<N
      *
      * This kernel is auto-generated at compile time for given values of K and M.
@@ -51,7 +64,17 @@ extern "C" {
      * @return ::GHOST_SUCCESS on success or an error indicator.
      */
     ghost_error_t ghost_tsmm(ghost_densemat_t *x, ghost_densemat_t *v, ghost_densemat_t *w, void *alpha, void *beta);
+    /**
+     * @brief Generate the map of auto-generated tsmm kernels.
+     */
     void ghost_tsmm_kernelmap_generate();
+    /**
+     * @brief Get the auto-generated tsmm kernel which fits the given parameters or, if not found, a fallback kernel. 
+     *
+     * @param[in] p The tsmm kernel parameters
+     *
+     * @return The according kernel. 
+     */
     tsmm_kernel ghost_tsmm_kernel(ghost_tsmm_parameters_t p);
 
 #ifdef __cplusplus
