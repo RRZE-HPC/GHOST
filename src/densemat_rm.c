19eb910b06fb5d34f146c0a072efbfef3efaaedb
Moritz Kreutzer <moritz.kreutzer@rrze.fau.de>
2014-10-22 14:23:24 +0200

    small fix

--- src/densemat_rm.c
+++ src/densemat_rm.c
@@ -397,7 +397,7 @@ static ghost_error_t vec_rm_viewPlain (ghost_densemat_t *vec, void *data, ghost_
     ghost_lidx_t v;
 
     if (vec->traits.flags & GHOST_DENSEMAT_DEVICE) {
-#if GHOST_HAVE_CUDA
+#ifdef GHOST_HAVE_CUDA
         INFO_LOG("The plain memory has to be valid CUDA device memory!");
         INFO_LOG("The column offset is being ignored!");
         vec->cu_val = &((char *)data)[lda*roffs*vec->elSize];
