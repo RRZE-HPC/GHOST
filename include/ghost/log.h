6a31a871644e7230104596f833df2a7250272499
Moritz Kreutzer <moritz.kreutzer@rrze.fau.de>
2014-12-02 10:26:10 +0100

    changed PERFWARNING logging color to MAGENTA

--- include/ghost/log.h
+++ include/ghost/log.h
@@ -73,12 +73,12 @@
 #ifdef GHOST_ERR_WARN_INFO_PRINTONCE
 #define INFO_LOG(...) {static int __printed = 0; if(!__printed && GHOST_VERBOSITY) { LOG(INFO,ANSI_COLOR_BLUE,__VA_ARGS__); __printed=1; }}
 #define WARNING_LOG(...) {static int __printed = 0; if(!__printed && GHOST_VERBOSITY) { LOG(WARNING,ANSI_COLOR_YELLOW,__VA_ARGS__); __printed=1; }}
-#define PERFWARNING_LOG(...) {static int __printed = 0; if(!__printed && GHOST_VERBOSITY) { LOG(PERFWARNING,ANSI_COLOR_GREEN,__VA_ARGS__); __printed=1; }}
+#define PERFWARNING_LOG(...) {static int __printed = 0; if(!__printed && GHOST_VERBOSITY) { LOG(PERFWARNING,ANSI_COLOR_MAGENTA,__VA_ARGS__); __printed=1; }}
 #define ERROR_LOG(...) {static int __printed = 0; if(!__printed && GHOST_VERBOSITY) { LOG(ERROR,ANSI_COLOR_RED,__VA_ARGS__); __printed=1; }}
 #else
 #define INFO_LOG(...) {if (GHOST_VERBOSITY) { LOG(INFO,ANSI_COLOR_BLUE,__VA_ARGS__); }}
 #define WARNING_LOG(...) {if (GHOST_VERBOSITY) { LOG(WARNING,ANSI_COLOR_YELLOW,__VA_ARGS__); }}
-#define PERFWARNING_LOG(...) {if (GHOST_VERBOSITY) { LOG(PERFWARNING,ANSI_COLOR_GREEN,__VA_ARGS__); }}
+#define PERFWARNING_LOG(...) {if (GHOST_VERBOSITY) { LOG(PERFWARNING,ANSI_COLOR_MAGENTA,__VA_ARGS__); }}
 #define ERROR_LOG(...) {if (GHOST_VERBOSITY) { LOG(ERROR,ANSI_COLOR_RED,__VA_ARGS__); }}
 #endif
 
