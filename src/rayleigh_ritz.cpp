af3887fe8bd46574708131e380f4d526794b4471
Moritz Kreutzer <moritz.kreutzer@rrze.fau.de>
2015-04-14 10:15:04 +0200

    wrong variables in UNUSED macros

--- src/rayleigh_ritz.cpp
+++ src/rayleigh_ritz.cpp
@@ -275,8 +275,13 @@ ghost_error_t ghost_grayleigh_ritz(ghost_sparsemat_t * mat, void * eigs, void * 
 #else
 ghost_error_t ghost_rayleigh_ritz(ghost_sparsemat_t * mat, void * eigs, void * res,  ghost_densemat_t * v_eigs , ghost_densemat_t * v_res, int obtion, ghost_spmv_flags_t spMVM_Options)
 {
-    UNUSED(v_ot);
-    UNUSED(v);
+    UNUSED(mat);
+    UNUSED(eigs);
+    UNUSED(res);
+    UNUSED(v_eigs);
+    UNUSED(v_res);
+    UNUSED(obtion);
+    UNUSED(spMVM_Options);
     ERROR_LOG("LAPACKE not found!");
     return GHOST_ERR_NOT_IMPLEMENTED;
 }
