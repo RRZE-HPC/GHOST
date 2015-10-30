36836a98374a10edada2765423339a8cbd340ac3
Moritz Kreutzer <moritz.kreutzer@rrze.fau.de>
2015-10-30 21:48:26 +0100

    better info output

--- src/sell_spmv.cpp
+++ src/sell_spmv.cpp
@@ -425,12 +425,12 @@ extern "C" ghost_error_t ghost_sell_spmv_selector(ghost_sparsemat_t *mat,
     p.storage = rhs->traits.storage;
     p.chunkheight = SELL(mat)->chunkHeight;
     
-    INFO_LOG("Initial search for SELL SpMV kernel with %d %d %d %d %d %d",p.alignment,p.impl,p.chunkheight,p.blocksz,p.vdt,p.mdt);
     if (!(lhs->traits.flags & GHOST_DENSEMAT_VIEW)) {
         p.blocksz = rhs->traits.ncols;
     } else {
         p.blocksz = rhs->traits.ncolspadded;
     }
+    INFO_LOG("Initial search for SELL SpMV kernel with alignment=%d impl=%d C=%d blocksz=%d vdt=%d mdt=%d",p.alignment,p.impl,p.chunkheight,p.blocksz,p.vdt,p.mdt);
 
     if (p.storage == GHOST_DENSEMAT_ROWMAJOR && p.blocksz == 1 && 
             rhs->stride == 1 && lhs->stride == 1) {
