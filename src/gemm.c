ea76bbb81902fb2662b2ebd74a895aba3e5f73d2
Moritz Kreutzer <moritz.kreutzer@rrze.fau.de>
2015-11-04 17:13:51 +0100

    this seems to work better: conjugating both input vectors instead of the result. obviously, this is very costly but it is just a matter of time until we have our own CUDA TSMTTSM kernels in GHOST

--- src/gemm.c
+++ src/gemm.c
@@ -227,7 +227,7 @@ ghost_densemat_t *w_in, const char *transw_in, void *alpha, void *beta, int redu
         culdv = *ldv;
         culdw = *ldw;
         culdx = *ldx;
-        bool requires_conjx = false;
+        bool requires_conj_vw = false;
 
         void *xcuval;
         void *vcuval;
@@ -285,8 +285,10 @@ ghost_densemat_t *w_in, const char *transw_in, void *alpha, void *beta, int redu
              * (X ) = (V^T) * (W^T)^T
              */
 
-            if (cutransv == CUBLAS_OP_C) {
-                requires_conjx = true;
+            if (!strncasecmp(transv_in,"C",1)) {
+                requires_conj_vw = true;
+                v->conj(v);
+                w->conj(w);
             }
 
             cutransw = cutransv; 
@@ -351,17 +353,19 @@ ghost_densemat_t *w_in, const char *transw_in, void *alpha, void *beta, int redu
             if (v->traits.datatype & GHOST_DT_DOUBLE) 
             {
                 CUBLAS_CALL_GOTO(cublasZgemm(ghost_cublas_handle,cutransv,cutransw,*m,*n,*k,(cuDoubleComplex *)alpha,(cuDoubleComplex *)vcuval,culdv,(cuDoubleComplex *)wcuval,culdw,(cuDoubleComplex *)mybeta,(cuDoubleComplex *)xcuval,culdx),err,ret);
-                if (requires_conjx) {
-                    INFO_LOG("Post-conjugating the result!");
-                    x->conj(x);
+                if (requires_conj_vw) {
+                    INFO_LOG("Conjugate input densemats back!");
+                    v->conj(v);
+                    w->conj(w);
                 }
             } 
             else 
             {
                 CUBLAS_CALL_GOTO(cublasCgemm(ghost_cublas_handle,cutransv,cutransw,*m,*n,*k,(cuFloatComplex *)alpha,(cuFloatComplex *)vcuval,culdv,(cuFloatComplex *)wcuval,culdw,(cuFloatComplex *)mybeta,(cuFloatComplex *)xcuval,culdx),err,ret);
-                if (requires_conjx) {
-                    INFO_LOG("Post-conjugating the result!");
-                    x->conj(x);
+                if (requires_conj_vw) {
+                    INFO_LOG("Conjugate input densemats back!");
+                    v->conj(v);
+                    w->conj(w);
                 }
             }
         } 
