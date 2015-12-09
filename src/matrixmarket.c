RRZE-HPC/GHOST

commit 8bf848df099875b4cd12212fc219a2ad2c483c4f

correct value casting for MM files, re #180

src/matrixmarket.c
@@ -110,9 +110,17 @@ int ghost_sparsemat_rowfunc_mm(ghost_gidx_t row, ghost_lidx_t *rowlen, ghost_gid
 
         for (i = 0; i < nz; ++i){
             if (matdt & GHOST_DT_COMPLEX) {
-                fscanf(f, "%"PRGIDX" %"PRGIDX" %lg %lg\n", &readrow,&readcol,(double *)value,(double *)(value+dtsize/2));
+                if (matdt & GHOST_DT_DOUBLE) {
+                    fscanf(f, "%"PRGIDX" %"PRGIDX" %lg %lg\n", &readcol,&readrow,(double *)value,(double *)(value+dtsize/2));
+                } else {
+                    fscanf(f, "%"PRGIDX" %"PRGIDX" %g %g\n", &readcol,&readrow,(double *)value,(double *)(value+dtsize/2));
+                }
             } else {
-                fscanf(f, "%"PRGIDX" %"PRGIDX" %lg\n", &readrow,&readcol,(double *)value);
+                if (matdt & GHOST_DT_DOUBLE) {
+                    fscanf(f, "%"PRGIDX" %"PRGIDX" %lg\n", &readcol,&readrow,(double *)value);
+                } else {
+                    fscanf(f, "%"PRGIDX" %"PRGIDX" %g\n", &readcol,&readrow,(double *)value);
+                }
             }
             readcol--;
             readrow--;
@@ -139,9 +147,17 @@ int ghost_sparsemat_rowfunc_mm(ghost_gidx_t row, ghost_lidx_t *rowlen, ghost_gid
 
             for (i = 0; i < nz; ++i){
                 if (matdt & GHOST_DT_COMPLEX) {
-                    fscanf(f, "%"PRGIDX" %"PRGIDX" %lg %lg\n", &readrow,&readcol,(double *)value,(double *)(value+dtsize/2));
+                    if (matdt & GHOST_DT_DOUBLE) {
+                        fscanf(f, "%"PRGIDX" %"PRGIDX" %lg %lg\n", &readcol,&readrow,(double *)value,(double *)(value+dtsize/2));
+                    } else {
+                        fscanf(f, "%"PRGIDX" %"PRGIDX" %g %g\n", &readcol,&readrow,(double *)value,(double *)(value+dtsize/2));
+                    }
                 } else {
-                    fscanf(f, "%"PRGIDX" %"PRGIDX" %lg\n", &readrow,&readcol,(double *)value);
+                    if (matdt & GHOST_DT_DOUBLE) {
+                        fscanf(f, "%"PRGIDX" %"PRGIDX" %lg\n", &readcol,&readrow,(double *)value);
+                    } else {
+                        fscanf(f, "%"PRGIDX" %"PRGIDX" %g\n", &readcol,&readrow,(double *)value);
+                    }
                 }
                 readrow--;
                 readcol--;
@@ -286,9 +302,17 @@ int ghost_sparsemat_rowfunc_mm_transpose(ghost_gidx_t row, ghost_lidx_t *rowlen,
 
         for (i = 0; i < nz; ++i){
             if (matdt & GHOST_DT_COMPLEX) {
-                fscanf(f, "%"PRGIDX" %"PRGIDX" %lg %lg\n", &readcol,&readrow,(double *)value,(double *)(value+dtsize/2));
+                if (matdt & GHOST_DT_DOUBLE) {
+                    fscanf(f, "%"PRGIDX" %"PRGIDX" %lg %lg\n", &readcol,&readrow,(double *)value,(double *)(value+dtsize/2));
+                } else {
+                    fscanf(f, "%"PRGIDX" %"PRGIDX" %g %g\n", &readcol,&readrow,(double *)value,(double *)(value+dtsize/2));
+                }
             } else {
-                fscanf(f, "%"PRGIDX" %"PRGIDX" %lg\n", &readcol,&readrow,(double *)value);
+                if (matdt & GHOST_DT_DOUBLE) {
+                    fscanf(f, "%"PRGIDX" %"PRGIDX" %lg\n", &readcol,&readrow,(double *)value);
+                } else {
+                    fscanf(f, "%"PRGIDX" %"PRGIDX" %g\n", &readcol,&readrow,(double *)value);
+                }
             }
             readcol--;
             readrow--;
@@ -315,9 +339,17 @@ int ghost_sparsemat_rowfunc_mm_transpose(ghost_gidx_t row, ghost_lidx_t *rowlen,
 
             for (i = 0; i < nz; ++i){
                 if (matdt & GHOST_DT_COMPLEX) {
-                    fscanf(f, "%"PRGIDX" %"PRGIDX" %lg %lg\n", &readcol,&readrow,(double *)value,(double *)(value+dtsize/2));
+                    if (matdt & GHOST_DT_DOUBLE) {
+                        fscanf(f, "%"PRGIDX" %"PRGIDX" %lg %lg\n", &readcol,&readrow,(double *)value,(double *)(value+dtsize/2));
+                    } else {
+                        fscanf(f, "%"PRGIDX" %"PRGIDX" %g %g\n", &readcol,&readrow,(double *)value,(double *)(value+dtsize/2));
+                    }
                 } else {
-                    fscanf(f, "%"PRGIDX" %"PRGIDX" %lg\n", &readcol,&readrow,(double *)value);
+                    if (matdt & GHOST_DT_DOUBLE) {
+                        fscanf(f, "%"PRGIDX" %"PRGIDX" %lg\n", &readcol,&readrow,(double *)value);
+                    } else {
+                        fscanf(f, "%"PRGIDX" %"PRGIDX" %g\n", &readcol,&readrow,(double *)value);
+                    }
                 }
                 readrow--;
                 readcol--;

commit 2fce25c38d9b7f1b37c3f05d63634ee444ce983d

fixed stupid copy&paste error which was introduced in 8bf848d. fixes #226. thanks for pointing thos one out Jonas!

src/matrixmarket.c
@@ -111,15 +111,15 @@ int ghost_sparsemat_rowfunc_mm(ghost_gidx_t row, ghost_lidx_t *rowlen, ghost_gid
         for (i = 0; i < nz; ++i){
             if (matdt & GHOST_DT_COMPLEX) {
                 if (matdt & GHOST_DT_DOUBLE) {
-                    fscanf(f, "%"PRGIDX" %"PRGIDX" %lg %lg\n", &readcol,&readrow,(double *)value,(double *)(value+dtsize/2));
+                    fscanf(f, "%"PRGIDX" %"PRGIDX" %lg %lg\n", &readrow,&readcol,(double *)value,(double *)(value+dtsize/2));
                 } else {
-                    fscanf(f, "%"PRGIDX" %"PRGIDX" %g %g\n", &readcol,&readrow,(float *)value,(float *)(value+dtsize/2));
+                    fscanf(f, "%"PRGIDX" %"PRGIDX" %g %g\n", &readrow,&readcol,(float *)value,(float *)(value+dtsize/2));
                 }
             } else {
                 if (matdt & GHOST_DT_DOUBLE) {
-                    fscanf(f, "%"PRGIDX" %"PRGIDX" %lg\n", &readcol,&readrow,(double *)value);
+                    fscanf(f, "%"PRGIDX" %"PRGIDX" %lg\n", &readrow,&readcol,(double *)value);
                 } else {
-                    fscanf(f, "%"PRGIDX" %"PRGIDX" %g\n", &readcol,&readrow,(float *)value);
+                    fscanf(f, "%"PRGIDX" %"PRGIDX" %g\n", &readrow,&readcol,(float *)value);
                 }
             }
             readcol--;
@@ -148,15 +148,15 @@ int ghost_sparsemat_rowfunc_mm(ghost_gidx_t row, ghost_lidx_t *rowlen, ghost_gid
             for (i = 0; i < nz; ++i){
                 if (matdt & GHOST_DT_COMPLEX) {
                     if (matdt & GHOST_DT_DOUBLE) {
-                        fscanf(f, "%"PRGIDX" %"PRGIDX" %lg %lg\n", &readcol,&readrow,(double *)value,(double *)(value+dtsize/2));
+                        fscanf(f, "%"PRGIDX" %"PRGIDX" %lg %lg\n", &readrow,&readcol,(double *)value,(double *)(value+dtsize/2));
                     } else {
-                        fscanf(f, "%"PRGIDX" %"PRGIDX" %g %g\n", &readcol,&readrow,(float *)value,(float *)(value+dtsize/2));
+                        fscanf(f, "%"PRGIDX" %"PRGIDX" %g %g\n", &readrow,&readcol,(float *)value,(float *)(value+dtsize/2));
                     }
                 } else {
                     if (matdt & GHOST_DT_DOUBLE) {
-                        fscanf(f, "%"PRGIDX" %"PRGIDX" %lg\n", &readcol,&readrow,(double *)value);
+                        fscanf(f, "%"PRGIDX" %"PRGIDX" %lg\n", &readrow,&readcol,(double *)value);
                     } else {
-                        fscanf(f, "%"PRGIDX" %"PRGIDX" %g\n", &readcol,&readrow,(float *)value);
+                        fscanf(f, "%"PRGIDX" %"PRGIDX" %g\n", &readrow,&readcol,(float *)value);
                     }
                 }
                 readrow--;