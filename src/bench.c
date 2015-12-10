9e824dfc7c3f77bd6de4bcc79c173ed7f9611ccb
Moritz Kreutzer <moritz.kreutzer@rrze.fau.de>
2015-12-10 17:38:42 +0100

    typo

--- src/bench.c
+++ src/bench.c
@@ -27,7 +27,7 @@ ghost_error_t ghost_bench_stream(ghost_bench_stream_test_t test, double *bw)
     ghost_type_get(&mytype);
     
     if (mytype == GHOST_TYPE_CUDA) {
-#if GHOST_HAVE_CUDA
+#ifdef GHOST_HAVE_CUDA
         return ghost_cu_bench_stream(test,bw);
 #endif
     }
