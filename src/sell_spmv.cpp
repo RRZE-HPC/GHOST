0f61b5390759ae057369275cc8dfc02470bdb5cc
Moritz Kreutzer <moritz.kreutzer@rrze.fau.de>
2016-01-15 12:56:47 +0100

    enhanced SELL SpMV kernel selection

--- src/sell_spmv.cpp
+++ src/sell_spmv.cpp
@@ -443,8 +443,8 @@ extern "C" ghost_error_t ghost_sell_spmv_selector(ghost_sparsemat_t *mat,
         PERFWARNING_LOG("Use plain implementation for scattered views");
         opt_impl = GHOST_IMPLEMENTATION_PLAIN;
     } else {
-        if (rhs->traits.ncols > 1 && rhs->traits.storage == GHOST_DENSEMAT_ROWMAJOR) {
-            opt_impl = ghost_get_best_implementation_for_bytesize(rhs->traits.ncols*rhs->elSize);
+        if (rhs->stride > 1 && rhs->traits.storage == GHOST_DENSEMAT_ROWMAJOR) {
+            opt_impl = ghost_get_best_implementation_for_bytesize(PAD(rhs->traits.ncols*rhs->elSize,ghost_machine_simd_width()));
         } else {
             opt_impl = ghost_get_best_implementation_for_bytesize(mat->traits.C*mat->elSize);
         }
