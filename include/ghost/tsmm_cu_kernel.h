1c372e2538094af794afe949e69c776a8c33b1ea
Moritz Kreutzer <moritz.kreutzer@rrze.fau.de>
2016-04-08 12:30:32 +0200

    enhanced performance of CUDA TSMM, re #175

--- include/ghost/tsmm_cu_kernel.h
+++ include/ghost/tsmm_cu_kernel.h
@@ -13,23 +13,20 @@
 template<typename T,int M, int K, bool betaiszero> 
 static __global__ void ghost_tsmm_cu_rm_cm(T * const __restrict__ x, const T * const __restrict__ v, const T * const __restrict__ w, const T alpha, const T beta, ghost_lidx nrows, ghost_lidx stridex, ghost_lidx stridev, ghost_lidx stridew)
 {
-    int row = blockIdx.x*blockDim.x+threadIdx.x;
-    int k;
+    int row = blockIdx.x*blockDim.y+threadIdx.y;
     int m;
     T tmp;
 
-    for (;row < nrows; row+=gridDim.x*blockDim.x) {
-        for (k=0; k<K; k++) {
-            if (betaiszero) {
-                zero<T>(tmp);
-            } else {
-                tmp = scale<T>(x[row*stridex+k],beta);
-            }
-            for (m=0; m<M; m++) {
-                tmp = axpy<T,T>(tmp,alpha,scale<T>(v[row*stridev+m],w[k*stridew+m]));
-            }
-            x[row*stridex+k] = tmp;
+    for (;row < nrows; row+=gridDim.x*blockDim.y) {
+        if (betaiszero) {
+            zero<T>(tmp);
+        } else {
+            tmp = scale<T>(x[row*stridex+threadIdx.x],beta);
         }
+        for (m=0; m<M; m++) {
+            tmp = axpy<T,T>(tmp,alpha,scale<T>(v[row*stridev+m],w[threadIdx.x*stridew+m]));
+        }
+        x[row*stridex+threadIdx.x] = tmp;
     }
 }
 
