191e1447eea0630e832efa21ced6b53c356fdfd6
Moritz Kreutzer <moritz.kreutzer@rrze.fau.de>
2016-04-14 14:37:01 +0200

    some work on CUDA TSMM, re #175

--- include/ghost/tsmm_cu_kernel.h
+++ include/ghost/tsmm_cu_kernel.h
@@ -9,25 +9,63 @@
 #include "ghost/config.h"
 #include "ghost/types.h"
 #include "ghost/cu_complex.h"
+#include "ghost/cu_sell_kernel.h"
+
+extern __shared__ char shared[];
 
 template<typename T,int M, int K, bool betaiszero> 
 static __global__ void ghost_tsmm_cu_rm_cm(T * const __restrict__ x, const T * const __restrict__ v, const T * const __restrict__ w, const T alpha, const T beta, ghost_lidx nrows, ghost_lidx stridex, ghost_lidx stridev, ghost_lidx stridew)
 {
-    int row = blockIdx.x*blockDim.y+threadIdx.y;
-    int m;
-    T tmp;
+    if (M >= K && !(M%K)) {
+        const int xheight = blockDim.x/K;
+        T * shmem = (T *)shared;
+        const int firstrowofblock = blockIdx.x*xheight;
+        const int rowinblock = threadIdx.x/K;
+        const int xrow = firstrowofblock + rowinblock;
+        const int xcol = threadIdx.x%K;
+        int m;
+        T tmp;
+        
+        int i;
 
-    for (;row < nrows; row+=gridDim.x*blockDim.y) {
+#pragma unroll
+        for (i=0; i<M/K; i++) {
+            shmem[i*blockDim.x+threadIdx.x] = v[firstrowofblock*stridev+i*blockDim.x+threadIdx.x];
+        }
+
         if (betaiszero) {
             zero<T>(tmp);
         } else {
-            tmp = scale<T>(x[row*stridex+threadIdx.x],beta);
+            tmp = scale<T>(x[xrow*stridex+threadIdx.x],beta);
         }
+
+#pragma unroll
         for (m=0; m<M; m++) {
-            tmp = axpy<T,T>(tmp,alpha,scale<T>(v[row*stridev+m],w[threadIdx.x*stridew+m]));
+            tmp = axpy<T,T>(tmp,alpha,scale<T>(shmem[rowinblock*M+m],w[xcol*stridew+m]));
         }
-        x[row*stridex+threadIdx.x] = tmp;
+
+        x[xrow*stridex+xcol] = tmp;
+
+    } else {
+
+        int row = blockIdx.x*blockDim.y+threadIdx.y;
+        int m;
+        T tmp;
+
+        for (;row < nrows; row+=gridDim.x*blockDim.y) {
+            if (betaiszero) {
+                zero<T>(tmp);
+            } else {
+                tmp = scale<T>(x[row*stridex+threadIdx.x],beta);
+            }
+#pragma unroll
+            for (m=0; m<M; m++) {
+                tmp = axpy<T,T>(tmp,alpha,scale<T>(v[row*stridev+m],w[threadIdx.x*stridew+m]));
+            }
+            x[row*stridex+threadIdx.x] = tmp;
+        }
     }
+    
 }
 
 #endif
