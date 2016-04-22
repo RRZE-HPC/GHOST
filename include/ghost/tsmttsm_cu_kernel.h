ff08df27f1b505970691144e2ee35f41ddc10d85
Dominik Ernst <dominik.ernst@fau.de>
2016-04-22 17:30:17 +0200

    tsmttsm_cu: optimized kernel, self multiplication optimization, automatic fallthrough to cublas

--- include/ghost/tsmttsm_cu_kernel.h
+++ include/ghost/tsmttsm_cu_kernel.h
@@ -10,72 +10,83 @@
 #include "ghost/config.h"
 #include "ghost/types.h"
 #include "ghost/cu_complex.h"
+#include <cublas_v2.h>
+#include <typeinfo>
+#include <iostream>
 
 namespace {
 
 void *temp_storage = NULL;
 size_t temp_storage_bytes = 0;
 
-namespace GENV3 {
-
+namespace SPECSMALL {
 template <typename T, int M, int N>
 __global__ void deviceReduce(T *blockResults, T *result, T alpha, T beta,
                              int blockCount, size_t lda, size_t ldb,
                              size_t ldc) {
   size_t tidx = blockDim.x * blockIdx.x + threadIdx.x;
-
   if (tidx >= M * N) return;
-
   int n = tidx / M;
   int m = tidx % M;
 
   T sum;
   zero(sum);
   for (int i = 0; i < blockCount; i++) {
-    sum = accu(sum,blockResults[i * N * ldc + n * ldc + m]);
+    sum = accu(sum, blockResults[i * N * ldc + n * ldc + m]);
   }
 
   result[n * ldc + m] = axpby(sum, result[n * ldc + m], alpha, beta);
 }
 
-template <typename T, bool conjv, int M, int N, int BLOCKSIZE, bool TRANSPOSE>
-__global__ void blockProductKernel(const T *A, const T *B, T *out, size_t K,
-                                   size_t lda, size_t ldb, size_t ldc) {
-  size_t tidx = blockDim.x * blockIdx.x + threadIdx.x;
+template <typename T, bool conjv, bool TRANSPOSE>
+__device__ T condConj1(T v) {
+  if (conjv && !TRANSPOSE) v = conj(v);
+  return v;
+}
+template <typename T, bool conjv, bool TRANSPOSE>
+__device__ T condConj2(T v) {
+  if (conjv && TRANSPOSE) v = conj(v);
+  return v;
+}
+
+template <typename T, bool conjv, int M, int N, int BLOCKSIZE, bool TRANSPOSE,
+          bool SELF>
+__global__ void blockProductKernel(const T *A, const T *B, T *out, const int K,
+                                   const int lda, const int ldb,
+                                   const int ldc) {
+  int tidx = blockDim.x * blockIdx.x + threadIdx.x;
+  int warpLane = threadIdx.x % 32;
+  int rowsPerWarp = 32 / M;
+  int m = warpLane % M;
+
+  if (warpLane >= rowsPerWarp * M) {
+    warpLane = rowsPerWarp * M - 1;
+    m = warpLane % M;
+  }
 
   __shared__ T blockStorage[BLOCKSIZE];
 
   zero(blockStorage[threadIdx.x]);
-
-  int m = tidx % M;
-
-  if (blockDim.x * gridDim.x / M == tidx / M) return;
 
   T threadSum[N];
   for (int n = 0; n < N; n++) {
     zero(threadSum[n]);
   }
 
-  if (conjv) {
-      if (TRANSPOSE) {
-          for (size_t idx = tidx / M; idx < K; idx += blockDim.x * gridDim.x / M) {
-            for (int n = 0; n < N; n++) {
-              threadSum[n] = axpy(threadSum[n], A[idx * lda + m], conj(B[idx * ldb + n]));
-            }
-          }
-      } else {
-          for (size_t idx = tidx / M; idx < K; idx += blockDim.x * gridDim.x / M) {
-            for (int n = 0; n < N; n++) {
-              threadSum[n] = axpy(threadSum[n], conj(A[idx * lda + m]), B[idx * ldb + n]);
-            }
-          }
-      }
-  } else {
-      for (size_t idx = tidx / M; idx < K; idx += blockDim.x * gridDim.x / M) {
-        for (int n = 0; n < N; n++) {
-          threadSum[n] = axpy(threadSum[n], A[idx * lda + m], B[idx * ldb + n]);
-        }
-      }
+  for (int idx = (tidx / 32) * rowsPerWarp + warpLane / M; idx < K;
+       idx += blockDim.x * gridDim.x / 32 * rowsPerWarp) {
+    T av = A[idx * lda + m];
+    if (!SELF) {
+      blockStorage[threadIdx.x] = B[idx * ldb + m];
+    } else {
+      blockStorage[threadIdx.x] = av;
+    }
+    int localAddress = threadIdx.x - m;
+    for (int n = 0; n < N; n++) {
+      threadSum[n] =
+          axpy(threadSum[n], condConj1<T, conjv, TRANSPOSE>(av),
+               condConj2<T, conjv, TRANSPOSE>(blockStorage[localAddress + n]));
+    }
   }
 
   for (int n = 0; n < N; n++) {
@@ -86,8 +97,10 @@ __global__ void blockProductKernel(const T *A, const T *B, T *out, size_t K,
     if (threadIdx.x < M) {
       T blockSum;
       zero(blockSum);
-      for (int i = threadIdx.x; i < BLOCKSIZE; i += M) {
-        blockSum = accu(blockSum,blockStorage[i]);
+      for (int w = 0; w < BLOCKSIZE / 32; w++) {
+        for (int wp = threadIdx.x; wp < rowsPerWarp * M; wp += M) {
+          blockSum = accu(blockSum, blockStorage[w * 32 + wp]);
+        }
       }
       if (TRANSPOSE) {
         out[blockIdx.x * M * ldc + m * ldc + n] = blockSum;
@@ -98,15 +111,47 @@ __global__ void blockProductKernel(const T *A, const T *B, T *out, size_t K,
   }
 }
 }
+
+
+cublasHandle_t handle;
 }
 
 template <typename T, int M, int N, int conjv>
-static void ghost_tsmttsm_cu_rm(T* const __restrict__ C,
-                                   const T* const __restrict__ A,
-                                   const T* const __restrict__ B, const T alpha,
-                                   const T beta, ghost_lidx K,
-                                   ghost_lidx ldc, ghost_lidx lda,
-                                   ghost_lidx ldb) {
+static void ghost_tsmttsm_cu_rm(T *const __restrict__ C,
+                                const T *const __restrict__ A,
+                                const T *const __restrict__ B, const T alpha,
+                                const T beta, ghost_lidx K, ghost_lidx ldc,
+                                ghost_lidx lda, ghost_lidx ldb) {
+  if (M > 32 || N > 32) {
+    if (temp_storage == NULL && temp_storage_bytes == 0) {
+      temp_storage_bytes = 8;
+      cublasCreate(&handle);
+    }
+
+    cublasStatus_t status;
+    cublasOperation_t op = (conjv == 1) ? CUBLAS_OP_C : CUBLAS_OP_T;
+    if (typeid(T) == typeid(double)) {
+      status = cublasDgemm(handle, CUBLAS_OP_N, CUBLAS_OP_T, M, N, K,
+                           (double *)&alpha, (double *)A, lda, (double *)B, ldb,
+                           (double *)&beta, (double *)C, ldc);
+    } else if (typeid(T) == typeid(float)) {
+      status = cublasSgemm(handle, CUBLAS_OP_N, CUBLAS_OP_T, M, N, K,
+                           (float *)&alpha, (float *)A, lda, (float *)B, ldb,
+                           (float *)&beta, (float *)C, ldc);
+    } else if (typeid(T) == typeid(cuDoubleComplex)) {
+      status = cublasZgemm(handle, CUBLAS_OP_N, op, M, N, K,
+                           (cuDoubleComplex *)&alpha, (cuDoubleComplex *)A, lda,
+                           (cuDoubleComplex *)B, ldb, (cuDoubleComplex *)&beta,
+                           (cuDoubleComplex *)C, ldc);
+    } else if (typeid(T) == typeid(cuFloatComplex)) {
+      status = cublasCgemm(handle, CUBLAS_OP_N, op, M, N, K,
+                           (cuFloatComplex *)&alpha, (cuFloatComplex *)A, lda,
+                           (cuFloatComplex *)B, ldb, (cuFloatComplex *)&beta,
+                           (cuFloatComplex *)C, ldc);
+    }
+    if (status != CUBLAS_STATUS_SUCCESS) std::cerr << "cublasXgemm error\n";
+    return;
+  }
 
   const int threadsPerBlock = 256;
   int deviceUsed;
@@ -114,16 +159,24 @@ static void ghost_tsmttsm_cu_rm(T* const __restrict__ C,
   cudaDeviceProp prop;
   cudaGetDeviceProperties(&prop, deviceUsed);
   int numBlocks;
-  
+
+  const void *func = NULL;
+
   if (N > M) {
-    cudaOccupancyMaxActiveBlocksPerMultiprocessor(
-        &numBlocks, GENV3::blockProductKernel<T, conjv, N, M, threadsPerBlock, true>,
-        threadsPerBlock, 0);
+    func = SPECSMALL::blockProductKernel<T, conjv, M, N, threadsPerBlock, true,
+                                         false>;
   } else {
-    cudaOccupancyMaxActiveBlocksPerMultiprocessor(
-        &numBlocks, GENV3::blockProductKernel<T, conjv, M, N, threadsPerBlock, false>,
-        threadsPerBlock, 0);
+    if (M == N && A == B) {
+      func = SPECSMALL::blockProductKernel<T, conjv, M, N, threadsPerBlock,
+                                           false, true>;
+    } else {
+      func = SPECSMALL::blockProductKernel<T, conjv, M, N, threadsPerBlock,
+                                           false, false>;
+    }
   }
+  cudaOccupancyMaxActiveBlocksPerMultiprocessor(&numBlocks, func,
+                                                threadsPerBlock, 0);
+
   int blockCount = prop.multiProcessorCount * numBlocks;
 
   if (temp_storage_bytes == 0 || temp_storage == NULL) {
@@ -132,18 +185,24 @@ static void ghost_tsmttsm_cu_rm(T* const __restrict__ C,
     if (temp_storage == NULL) temp_storage_bytes = 0;
   }
   if (N > M) {
-    GENV3::blockProductKernel<T, conjv, N, M, threadsPerBlock,
-                              true><<<blockCount, threadsPerBlock>>>(
+    SPECSMALL::blockProductKernel<T, conjv, N, M, threadsPerBlock, true,
+                                  false><<<blockCount, threadsPerBlock>>>(
         B, A, (T *)temp_storage, K, ldb, lda, ldc);
   } else {
-    GENV3::blockProductKernel<T, conjv, M, N, threadsPerBlock,
-                              false><<<blockCount, threadsPerBlock>>>(
-        A, B, (T *)temp_storage, K, lda, ldb, ldc);
+    if (M == N && A == B) {
+      SPECSMALL::blockProductKernel<T, conjv, M, N, threadsPerBlock, false,
+                                    true><<<blockCount, threadsPerBlock>>>(
+          A, B, (T *)temp_storage, K, lda, ldb, ldc);
+    } else {
+      SPECSMALL::blockProductKernel<T, conjv, M, N, threadsPerBlock, false,
+                                    false><<<blockCount, threadsPerBlock>>>(
+          A, B, (T *)temp_storage, K, lda, ldb, ldc);
+    }
   }
 
-  GENV3::deviceReduce<T, M, N><<<M * N / 256 + 1, 256>>>(
+  SPECSMALL::deviceReduce<T, M, N><<<M * N / 256 + 1, 256>>>(
       (T *)temp_storage, C, alpha, beta, blockCount, lda, ldb, ldc);
-  
+
   ghost_cu_free(temp_storage);
   temp_storage = NULL;
   temp_storage_bytes = 0;
