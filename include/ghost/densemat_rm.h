a95d6f9013f658fdf3f3e5f5d0dab636768d1566
Moritz Kreutzer <moritz.kreutzer@rrze.fau.de>
2016-04-28 09:14:18 +0200

    Revert "Revert "implemented HOST&DEVICE computation for all densemat functions which are accessed via function pointers, re #246. the host kernel is called between calling the device kernel and waiting for it to finish, so the two kernels should be overlapping. this mechanism still has to be implemented for ghost_gemm() if required.""
    This reverts commit b1f20cf0d75ef6b1a8c58732e6877ab3d5d38a3c.

--- include/ghost/densemat_rm.h
+++ include/ghost/densemat_rm.h
@@ -28,20 +28,24 @@ extern "C" {
      */
     ghost_error ghost_densemat_rm_setfuncs(ghost_densemat *vec);
     ghost_error ghost_densemat_rm_malloc(ghost_densemat *vec, int* needInit);
-    ghost_error ghost_densemat_rm_string_selector(ghost_densemat *vec, char **str);
-    ghost_error ghost_densemat_rm_normalize_selector(ghost_densemat *vec);
-    ghost_error ghost_densemat_rm_dotprod_selector(ghost_densemat *vec, void *, ghost_densemat *);
-    ghost_error ghost_densemat_rm_vscale_selector(ghost_densemat *vec1, void *); 
-    ghost_error ghost_densemat_rm_vaxpy_selector(ghost_densemat *vec1, ghost_densemat *vec2, void *); 
-    ghost_error ghost_densemat_rm_vaxpby_selector(ghost_densemat *vec1, ghost_densemat *vec2, void *, void *); 
-    ghost_error ghost_densemat_rm_fromRand_selector(ghost_densemat *vec);
-    ghost_error ghost_densemat_rm_fromScalar_selector(ghost_densemat *vec, void *);
-    ghost_error ghost_densemat_rm_fromVec_selector(ghost_densemat *vec1, ghost_densemat *vec2, ghost_lidx, ghost_lidx); 
-    ghost_error ghost_densemat_rm_permute_selector(ghost_densemat *vec, ghost_permutation_direction dir);
-    ghost_error ghost_densemat_rm_norm_selector(ghost_densemat *vec, void *res, void *p);
-    ghost_error ghost_densemat_rm_averagehalo_selector(ghost_densemat *vec);
-    ghost_error ghost_densemat_rm_conj_selector(ghost_densemat *vec);
-    ghost_error ghost_densemat_rm_vaxpbypcz_selector(ghost_densemat *vec, ghost_densemat *vec2, void *scale, void *b, ghost_densemat *vec3, void *c);
+    ghost_error ghost_densemat_rm_string(ghost_densemat *vec, char **str);
+    ghost_error ghost_densemat_rm_normalize(ghost_densemat *vec);
+    ghost_error ghost_densemat_rm_dotprod(ghost_densemat *vec, void *, ghost_densemat *);
+    ghost_error ghost_densemat_rm_scale(ghost_densemat *vec1, void *); 
+    ghost_error ghost_densemat_rm_axpy(ghost_densemat *vec1, ghost_densemat *vec2, void *); 
+    ghost_error ghost_densemat_rm_axpby(ghost_densemat *vec1, ghost_densemat *vec2, void *, void *); 
+    ghost_error ghost_densemat_rm_axpbypcz(ghost_densemat *vec, ghost_densemat *vec2, void *scale, void *b, ghost_densemat *vec3, void *c);
+    ghost_error ghost_densemat_rm_vscale(ghost_densemat *vec1, void *); 
+    ghost_error ghost_densemat_rm_vaxpy(ghost_densemat *vec1, ghost_densemat *vec2, void *); 
+    ghost_error ghost_densemat_rm_vaxpby(ghost_densemat *vec1, ghost_densemat *vec2, void *, void *); 
+    ghost_error ghost_densemat_rm_vaxpbypcz(ghost_densemat *vec, ghost_densemat *vec2, void *scale, void *b, ghost_densemat *vec3, void *c);
+    ghost_error ghost_densemat_rm_fromRand(ghost_densemat *vec);
+    ghost_error ghost_densemat_rm_fromScalar(ghost_densemat *vec, void *);
+    ghost_error ghost_densemat_rm_fromVec(ghost_densemat *vec1, ghost_densemat *vec2, ghost_lidx, ghost_lidx); 
+    ghost_error ghost_densemat_rm_permute(ghost_densemat *vec, ghost_permutation_direction dir);
+    ghost_error ghost_densemat_rm_norm(ghost_densemat *vec, void *res, void *p);
+    ghost_error ghost_densemat_rm_averagehalo(ghost_densemat *vec);
+    ghost_error ghost_densemat_rm_conj(ghost_densemat *vec);
 #ifdef __cplusplus
 }
 #endif
