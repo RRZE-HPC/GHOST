4d2cab34ab3d9d457a4854a2c40d47811bf2bac4
Christie Louis Alappat <christiealappatt@gmail.com>
2016-05-04 13:06:04 +0200

    carp_rb now takes care of communication before and after back sweep

--- src/carp_rb.c
+++ src/carp_rb.c
@@ -14,31 +14,29 @@ ghost_error ghost_carp_rb(ghost_sparsemat *mat, ghost_densemat *x, ghost_densema
 
     ghost_densemat_halo_comm comm = GHOST_DENSEMAT_HALO_COMM_INITIALIZER;
     GHOST_CALL_RETURN(x->halocommInit(x,&comm));
-    GHOST_CALL_RETURN(x->halocommStart(x,&comm));
-    GHOST_CALL_RETURN(x->halocommFinalize(x,&comm));
-   
+  
     ghost_kacz_opts opts = GHOST_KACZ_OPTS_INITIALIZER;
     opts.omega = omega;
+
     opts.direction = GHOST_KACZ_DIRECTION_FORWARD;
-
+    GHOST_CALL_RETURN(x->halocommStart(x,&comm));
+    GHOST_CALL_RETURN(x->halocommFinalize(x,&comm));
     if(flag_rb!=0){
-   	 GHOST_CALL_RETURN(ghost_kacz_rb(x,mat,b,opts));
-   	 GHOST_CALL_RETURN(x->averageHalo(x));
-   	 opts.direction = GHOST_KACZ_DIRECTION_BACKWARD;
-         GHOST_CALL_RETURN(x->halocommStart(x,&comm));
-         GHOST_CALL_RETURN(x->halocommFinalize(x,&comm));
-   	 GHOST_CALL_RETURN(ghost_kacz_rb(x,mat,b,opts));
-         GHOST_CALL_RETURN(x->averageHalo(x));
+ 	GHOST_CALL_RETURN(ghost_kacz_rb(x,mat,b,opts));
+     } else {
+       	GHOST_CALL_RETURN(ghost_kacz(x,mat,b,opts)); 
+     }    
+     GHOST_CALL_RETURN(x->averageHalo(x));
 
-    } else {
-         GHOST_CALL_RETURN(ghost_kacz(x,mat,b,opts));
-         GHOST_CALL_RETURN(x->averageHalo(x));
-         opts.direction = GHOST_KACZ_DIRECTION_BACKWARD;
-         GHOST_CALL_RETURN(x->halocommStart(x,&comm));
-         GHOST_CALL_RETURN(x->halocommFinalize(x,&comm));
-         GHOST_CALL_RETURN(ghost_kacz(x,mat,b,opts));
-         GHOST_CALL_RETURN(x->averageHalo(x));
-    }
+     opts.direction = GHOST_KACZ_DIRECTION_BACKWARD;
+     GHOST_CALL_RETURN(x->halocommStart(x,&comm));
+     GHOST_CALL_RETURN(x->halocommFinalize(x,&comm));
+     if(flag_rb!=0){
+       	GHOST_CALL_RETURN(ghost_kacz_rb(x,mat,b,opts));
+     } else {
+       	GHOST_CALL_RETURN(ghost_kacz(x,mat,b,opts));   
+     }
+    GHOST_CALL_RETURN(x->averageHalo(x));
 
     GHOST_FUNC_EXIT(GHOST_FUNCTYPE_SOLVER);
     return GHOST_SUCCESS;
