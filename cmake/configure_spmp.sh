25c21600831bedf3f45b3afa478fb71982d55fef
Moritz Kreutzer <moritz.kreutzer@rrze.fau.de>
2016-09-19 13:50:03 +0200

    added a comment

--- cmake/configure_spmp.sh
+++ cmake/configure_spmp.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# This script includes the missing header x86intrin.h if compiled with GCC
 
 grep -q -F 'x86intrin' $1 || sed -i '1s/^/\#ifdef __GNUC__\n\#include <x86intrin.h>\n\#endif\n/' $1
 
