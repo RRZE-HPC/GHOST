4406d6600d65ca0beacf820c6f324bbd7287dc86
Melven Roehrig-Zoellner <melven.roehrig-zoellner@dlr.de>
2016-10-22 08:44:48 +0000

    Trying to use ccache in ci-sc-dlr.sh script

--- test/ci-sc-dlr.sh
+++ test/ci-sc-dlr.sh
@@ -49,12 +49,18 @@ module() { eval `/usr/bin/modulecmd bash $*`; }
 # load modules
 module load "PrgEnv/$PRGENV"
 # set compiler names
-set -- $(mpicc -show)
-export CC=$1
-set -- $(mpicxx -show)
-export CXX=$1
-set -- $(mpif90 -show)
-export FC=$1
+# use ccache to speed up build
+if [[ "$PRGENV" =~ gcc* ]]; then
+  export FC="ccache gfortran" CC="ccache gcc" CXX="ccache g++"
+elif [[ "$PRGENV" =~ intel* ]]; then
+  export FC=ifort CC=icc CXX=icpc
+fi
+#set -- $(mpicc -show)
+#export CC=$1
+#set -- $(mpicxx -show)
+#export CXX=$1
+#set -- $(mpif90 -show)
+#export FC=$1
 
 echo "compilers: CC=$CC, CXX=$CXX, FC=$FC"
 
