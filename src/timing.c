b840a51abf335d1be7d32e0ba8456484e18b3325
Moritz Kreutzer <moritz.kreutzer@fau.de>
2017-01-18 21:30:54 +0900

    use clock_gettime() for timing

--- src/timing.c
+++ src/timing.c
@@ -1,5 +1,6 @@
+#define _XOPEN_SOURCE 600
 #include <stdlib.h>
-#include <sys/time.h>
+#include <time.h>
 #include <errno.h>
 #include "ghost/timing.h"
 #include "ghost/func_util.h"
@@ -7,29 +8,29 @@
 
 ghost_error ghost_timing_wc(double *time)
 {
-    struct timeval tp;
+    struct timespec tp;
     int err;
-    err = gettimeofday(&tp, NULL);
+    err = clock_gettime(CLOCK_MONOTONIC,&tp);
     if (err) {
-        ERROR_LOG("Error in gettimeofday: %s",strerror(errno));
+        ERROR_LOG("Error in clock_gettime(): %s",strerror(errno));
         return GHOST_ERR_UNKNOWN;
     }
 
-    *time = (double)tp.tv_sec + (double)tp.tv_usec/1000000.0;
+    *time = (double)tp.tv_sec + (double)tp.tv_nsec/1.e9;
     return GHOST_SUCCESS;
 }
 
 ghost_error ghost_timing_wcmilli(double *time)
 {
-    struct timeval tp;
+    struct timespec tp;
     int err;
-    err = gettimeofday(&tp, NULL);
+    err = clock_gettime(CLOCK_MONOTONIC,&tp);
     if (err) {
-        ERROR_LOG("Error in gettimeofday: %s",strerror(errno));
+        ERROR_LOG("Error in clock_gettime(): %s",strerror(errno));
         return GHOST_ERR_UNKNOWN;
     }
 
-    *time = (double)tp.tv_sec*1000.0 + (double)tp.tv_usec/1000.0;
+    *time = (double)tp.tv_sec*1000.0 + (double)tp.tv_nsec/1.e6;
     return GHOST_SUCCESS;
 }
 
