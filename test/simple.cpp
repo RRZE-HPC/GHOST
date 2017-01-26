cf7ce261421e3128174e7e0a88d2ad22689c91df
Moritz Kreutzer <moritz.kreutzer@fau.de>
2017-01-26 14:21:14 +0100

    fixed simple test

--- test/simple.cpp
+++ test/simple.cpp
@@ -145,8 +145,8 @@ int main(int argc, char **argv) {
             ghost_lidx i;
 
             for (i=0; i<DM_NROWS(y); i++) {
-                GHOST_TEST_CALL(ghost_densemat_entry(&yent,y,i,0));
-                GHOST_TEST_CALL(ghost_densemat_entry(&xent,x,i,0));
+                GHOST_TEST_CALL(ghost_densemat_entry(yent,y,i,0));
+                GHOST_TEST_CALL(ghost_densemat_entry(xent,x,i,0));
                 ref_funcs_diag[pair<ghost_datatype,ghost_datatype>(vtraits_it->datatype,mtraits_it->datatype)](yent_ref,i,xent);
                 RETURN_IF_DIFFER((void *)yent,(void *)yent_ref,1,vtraits_it->datatype);
             }
