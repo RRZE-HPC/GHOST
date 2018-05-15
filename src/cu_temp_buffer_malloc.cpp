7cb52d511cda688a34553bc6ac40a2a3a4bc2c5b
Dominik Ernst <dominik.ernst@fau.de>
2018-05-15 12:23:04 +0200

    temp_buffer_malloc: use GHOST_HAVE_CUDA guards to fix compilation for non CUDA targets

--- src/cu_temp_buffer_malloc.cpp
+++ src/cu_temp_buffer_malloc.cpp
@@ -1,6 +1,7 @@
 #include "ghost/cu_temp_buffer_malloc.h"
 #include "ghost/error.h"
 #include "ghost/log.h"
+#include "ghost/util.h"
 #include <algorithm>
 #include <iostream>
 #include <mutex>
@@ -25,6 +26,7 @@ unsigned int peakBufferCount = 0;
 
 ghost_error ghost_cu_temp_buffer_malloc(void **mem, size_t bytesize)
 {
+#ifdef GHOST_HAVE_CUDA
     lock_guard<mutex> lock(cu_temp_buffer_malloc_mutex);
 
     peakBufferCount += 1;
@@ -52,12 +54,17 @@ ghost_error ghost_cu_temp_buffer_malloc(void **mem, size_t bytesize)
             buffers.erase(begin(buffers));
         }
     }
+#else
+    UNUSED(mem);
+    UNUSED(bytesize);
+#endif
 
     return GHOST_SUCCESS;
 }
 
 ghost_error ghost_cu_temp_buffer_free(void *mem)
 {
+#ifdef GHOST_HAVE_CUDA
     lock_guard<mutex> lock(cu_temp_buffer_malloc_mutex);
     peakBufferCount = peakBufferCount - 1;
 
@@ -71,5 +78,8 @@ ghost_error ghost_cu_temp_buffer_free(void *mem)
         return GHOST_ERR_UNKNOWN;
     }
 
+#else
+    UNUSED(mem);
+#endif
     return GHOST_SUCCESS;
 }
